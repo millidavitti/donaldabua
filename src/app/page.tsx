8572abb18cb90cec9a05dbf02424a88a89fced71
Donald <83835207+millidavitti@users.noreply.github.com>
2025-01-26 15:40:36 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Flex from "@/components/layouts/flex";
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<Flex className='w-full'>
+			<h1>App Template</h1>
+		</Flex>
+	);
+}
---
+++ src/components/layouts/flex.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/utils/cn";
+import { HTMLProps, ReactNode } from "react";
+
+interface FlexColumn {
+	children?: ReactNode;
+	className?: string;
+	htmlProps?: Partial<HTMLProps<HTMLDivElement>>;
+	flex?: "row" | "column";
+}
+export default function Flex({
+	children,
+	className,
+	htmlProps,
+	flex = "column",
+}: FlexColumn) {
+	return (
+		<>
+			{flex === "column" && (
+				<div
+					className={cn(
+						"flex flex-col p-3 outline-2 overflow-y-auto",
+						className,
+					)}
+					{...htmlProps}
+				>
+					{children}
+				</div>
+			)}
+			{flex === "row" && (
+				<div
+					className={cn(
+						"flex flex-row p-3 outline-2 overflow-x-auto",
+						className,
+					)}
+					{...htmlProps}
+				>
+					{children}
+				</div>
+			)}
+		</>
+	);
+}
