75b6f8eee970d663d816279ae4fbaedd8b468cff
davitti <abuadonald@gmail.com>
2025-01-27 02:11:39 +0100

    Remove Home component page.

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Flex from "@/components/layouts/flex";
-
-export default function Home() {
-	return (
-		<Flex className='w-full'>
-			<h1 className='outline'>Navigation Bar</h1>
-		</Flex>
-	);
-}
