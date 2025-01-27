5090fd254905b7937bf38f282a33cf124002f532
davitti <abuadonald@gmail.com>
2025-01-27 11:28:48 +0100

    Update Sidebar component layout styles

--- src/app/(dashboard)/components/sidebar.tsx
+++ src/app/(dashboard)/components/sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Sidebar() {
 	return (
-		<Flex className='grow gap-3' flex='column'>
+		<Flex className='basis-80 grow gap-3 shrink-0' flex='column'>
 			<h2>Sidebar</h2>
 			<Flex className='grow'></Flex>
 			<Flex className='grow'></Flex>
