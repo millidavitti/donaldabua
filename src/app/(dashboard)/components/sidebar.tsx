c7305ff0d26cf9d9b7119d116c090e133094c56b
davitti <abuadonald@gmail.com>
2025-01-28 00:17:07 +0100

    Add VideoIntroduction component to Sidebar

--- src/app/(dashboard)/components/sidebar.tsx
+++ src/app/(dashboard)/components/sidebar.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Flex from "@/components/layouts/flex";
+
 import React from "react";
+import VideoIntroduction from "./video-introduction";
 
 export default function Sidebar() {
 	return (
 		<Flex className='basis-80 grow gap-3 shrink-0' flex='column'>
 			<h2>Sidebar</h2>
-			<Flex className='grow'></Flex>
+			<VideoIntroduction />
 			<Flex className='grow'></Flex>
 		</Flex>
 	);
