003e97452bbe05a4a0f7caa987417b906dbd5ad8
davitti <abuadonald@gmail.com>
2025-01-27 01:26:16 +0100

    Add DeveloperSummary component to dashboard

---
+++ src/app/(dashboard)/components/developer-summary.tsx
@@ -0,0 +1,6 @@
+import Flex from "@/components/layouts/flex";
+import React from "react";
+
+export default function DeveloperSummary() {
+	return <Flex className='basis-40 grow shrink-0'>Developer Summary</Flex>;
+}
