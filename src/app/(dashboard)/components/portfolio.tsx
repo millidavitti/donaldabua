dc7304945ab1eb9aedcdf72883fe0d0b63b61d09
davitti <abuadonald@gmail.com>
2025-01-27 01:26:28 +0100

    Add Portfolio component to dashboard.

---
+++ src/app/(dashboard)/components/portfolio.tsx
@@ -0,0 +1,6 @@
+import Flex from "@/components/layouts/flex";
+import React from "react";
+
+export default function Portfolio() {
+	return <Flex className='basis-40 grow shrink-0'>Portfolio</Flex>;
+}
