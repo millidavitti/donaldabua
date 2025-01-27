b9ed43bd99ab4f72c5c9fc2c630de1e876d538cf
davitti <abuadonald@gmail.com>
2025-01-28 00:17:20 +0100

    Add 'video' property to Profile type and related atoms.

--- src/data/atoms/app_data.ts
+++ src/data/atoms/app_data.ts
@@ -4,6 +4,7 @@ import { focusAtom } from "jotai-optics";
 export type Profile = {
 	name: string;
 	image: string;
+	video: string | undefined;
 	location: {
 		city: string;
 		country: string;
@@ -12,6 +13,7 @@ export type Profile = {
 export const profile_jotai = atom<Profile>({
 	name: "Donald Abua",
 	image: "/stud.jpg",
+	video: undefined,
 	location: {
 		city: "New York",
 		country: "United State",
@@ -23,6 +25,9 @@ export const profile_name_jotai = focusAtom(profile_jotai, (optic) =>
 );
 export const profile_image_jotai = focusAtom(profile_jotai, (optic) =>
 	optic.prop("image"),
+);
+export const profile_video_jotai = focusAtom(profile_jotai, (optic) =>
+	optic.prop("video"),
 );
 export const profile_location_jotai = focusAtom(profile_jotai, (optic) =>
 	optic.prop("location"),
