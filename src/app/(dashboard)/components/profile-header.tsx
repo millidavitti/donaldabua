4f64da6a5285937f0273328b3b1a8d196130d351
davitti <abuadonald@gmail.com>
2025-01-27 18:17:35 +0100

    Remove unused profile data and logging from ProfileHeader component.

--- src/app/(dashboard)/components/profile-header.tsx
+++ src/app/(dashboard)/components/profile-header.tsx
@@ -4,11 +4,7 @@ import React from "react";
 import EditProfilePhoto from "./edit-profile-photo";
 import EditProfileName from "./edit-profile-name";
 import EditProfileLocation from "./edit-profile-location";
-import { useAtomValue } from "jotai";
-import { profile_jotai } from "@/data/atoms/app_data";
 export default function ProfileHeader() {
-	const profile = useAtomValue(profile_jotai);
-	console.log(profile);
 	return (
 		<Flex className='shrink-0 gap-3 overflow-visible flex-wrap'>
 			<EditProfilePhoto />
