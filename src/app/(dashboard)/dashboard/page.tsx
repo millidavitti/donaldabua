4ffdd1b7c404523f3966d2357a462521f7a2fbb6
davitti <abuadonald@gmail.com>
2025-01-27 01:27:07 +0100

    Add Dashboard component with layout and sections

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import Flex from "@/components/layouts/flex";
+import React from "react";
+import ProfileHeader from "../components/profile-header";
+import Sidebar from "../components/sidebar";
+import DeveloperOverview from "../components/developer-overview";
+import Testimonials from "../components/testimonials";
+import EmploymentHistory from "../components/employment-history";
+import Socials from "../components/socials";
+
+export default function Dashboard() {
+	return (
+		<Flex className='w-full h-full gap-3' flex='column'>
+			<h1 className='outline'>Dashboard</h1>
+			<Flex className='outline h-full gap-3' flex='column'>
+				<ProfileHeader />
+				{/* Main */}
+				<Flex className='h-full gap-3 shrink-0'>
+					<Sidebar />
+					<DeveloperOverview />
+				</Flex>
+				{/* Social Proof */}
+				<Flex className='h-full gap-3 shrink-0' flex='column'>
+					<Testimonials />
+					<EmploymentHistory />
+					<Socials />
+				</Flex>
+			</Flex>
+		</Flex>
+	);
+}
