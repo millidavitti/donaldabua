b4bfc4ca3e479fb4c306fad25632cac99243a265
davitti <abuadonald@gmail.com>
2025-01-27 15:19:26 +0100

    Add EditProfileName component for editing user profile name.

---
+++ src/app/(dashboard)/components/edit-profile-name.tsx
@@ -0,0 +1,39 @@
+import InteractiveIcon from "@/components/layouts/interactive_icon";
+import { profile_jotai } from "@/data/atoms/app_data";
+import { edit_profile_jotai } from "@/data/atoms/ui_state";
+import { useAtom, useAtomValue } from "jotai";
+import { X } from "lucide-react";
+import React from "react";
+
+export default function EditProfileName() {
+	const [edit_profile, edit_profile_setter] = useAtom(edit_profile_jotai);
+	const profile = useAtomValue(profile_jotai);
+
+	return (
+		<>
+			<h2
+				className='font-bold text-4xl cursor-pointer data-[is-visible=false]:absolute data-[is-visible=true]:hidden'
+				onClick={() => {
+					edit_profile_setter(
+						edit_profile === "edit-name" ? null : "edit-name",
+					);
+				}}
+			>
+				{profile.name}
+			</h2>
+			<form
+				className='flex outline bg-white data-[is-visible=true]:absolute data-[is-visible=false]:hidden'
+				data-is-visible={edit_profile === "edit-name"}
+				onSubmit={(e) => {
+					e.preventDefault();
+					edit_profile_setter(null);
+				}}
+			>
+				<input type='text' required className='outline-none p-3' />
+				<InteractiveIcon callback={() => edit_profile_setter(null)}>
+					<X className='stroke-light-error' />
+				</InteractiveIcon>
+			</form>
+		</>
+	);
+}
