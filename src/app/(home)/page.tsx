e753f2c13e51cf143dfa26ca15192dd36dce8863
davitti <abuadonald@gmail.com>
2025-01-27 02:11:47 +0100

    Refactor Home component to use Flex layout and update content.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import Flex from "@/components/layouts/flex";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<Flex className='w-full'>
+			<h1 className='outline'>Navigation Bar</h1>
+		</Flex>
+	);
 }
