c11c9a2ad6da3645b7cb194e688165a0764936cd
davitti <abuadonald@gmail.com>
2025-01-28 00:17:30 +0100

    Add "edit-video" option to edit_profile_jotai atom

--- src/data/atoms/ui_state.ts
+++ src/data/atoms/ui_state.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
 
 export const edit_profile_jotai = atom<
-	"edit-image" | "edit-location" | "edit-name" | null
+	"edit-image" | "edit-location" | "edit-name" | "edit-video" | null
 >(null);
