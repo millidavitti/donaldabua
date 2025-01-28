5dd21d418a29016c76c4d67c73ca849e585daba0
davitti <abuadonald@gmail.com>
2025-01-28 14:56:26 +0100

    Add hoursPerWeek property to Profile type and update atoms accordingly.

--- src/data/atoms/app_data.ts
+++ src/data/atoms/app_data.ts
@@ -9,6 +9,7 @@ export type Profile = {
 		city: string;
 		country: string;
 	};
+	hoursPerWeek: string;
 };
 export const profile_jotai = atom<Profile>({
 	name: "Donald Abua",
@@ -18,6 +19,7 @@ export const profile_jotai = atom<Profile>({
 		city: "New York",
 		country: "United State",
 	},
+	hoursPerWeek: "More than 30 hrs/week",
 });
 
 export const profile_name_jotai = focusAtom(profile_jotai, (optic) =>
@@ -31,4 +33,7 @@ export const profile_video_jotai = focusAtom(profile_jotai, (optic) =>
 );
 export const profile_location_jotai = focusAtom(profile_jotai, (optic) =>
 	optic.prop("location"),
+);
+export const profile_hours_per_week_jotai = focusAtom(profile_jotai, (optic) =>
+	optic.prop("hoursPerWeek"),
 );
