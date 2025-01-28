940137d7cc10a1cfa80d8b6eda6c9935b6d31cda
davitti <abuadonald@gmail.com>
2025-01-28 18:18:27 +0100

    Replace DeveloperSummary with EditDeveloperSummary in DeveloperOverview component.

--- src/app/(dashboard)/components/developer-overview.tsx
+++ src/app/(dashboard)/components/developer-overview.tsx
@@ -1,14 +1,13 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
-import DeveloperSummary from "./developer-summary";
+import EditDeveloperSummary from "./edit-developer-summary";
 import Portfolio from "./portfolio";
 import Skills from "./skills";
 
 export default function DeveloperOverview() {
 	return (
 		<Flex className='basis-[720px] grow-[2] gap-3' flex='column'>
-			<h2 className='shrink-0'>Developer Overview</h2>
-			<DeveloperSummary />
+			<EditDeveloperSummary />
 			<Portfolio />
 			<Skills />
 		</Flex>
