37c62b8744066f612668082948c9e1bafd1d62c6
davitti <abuadonald@gmail.com>
2025-01-28 15:41:21 +0100

    Rename DeveloperSummary import and update layout width.

--- src/app/(dashboard)/components/developer-overview.tsx
+++ src/app/(dashboard)/components/developer-overview.tsx
@@ -1,12 +1,12 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
-import DeveloperSummary from "./developer-summary";
+import DeveloperSummary from "./edit-developer-summary";
 import Portfolio from "./portfolio";
 import Skills from "./skills";
 
 export default function DeveloperOverview() {
 	return (
-		<Flex className='basis-[640px] grow-[2] gap-3' flex='column'>
+		<Flex className='basis-[720px] grow-[2] gap-3' flex='column'>
 			<h2 className='shrink-0'>Developer Overview</h2>
 			<DeveloperSummary />
 			<Portfolio />
