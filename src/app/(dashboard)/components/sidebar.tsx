c87930f60126153a81244b903a5d05d453e66e03
davitti <abuadonald@gmail.com>
2025-01-30 00:43:39 +0100

    Add EditSocials component to Sidebar.

--- src/app/(dashboard)/components/sidebar.tsx
+++ src/app/(dashboard)/components/sidebar.tsx
@@ -4,13 +4,15 @@ import Flex from "@/components/layouts/flex";
 import React from "react";
 import VideoIntroduction from "./video-introduction";
 import EditAvailability from "./edit-availability";
+import EditSocials from "./edit-socials";
 
 export default function Sidebar() {
 	return (
 		<Flex flex='column' className='grow gap-3 lg:shrink-0 lg:basis-80'>
 			<VideoIntroduction />
-			<Flex flex='column'>
+			<Flex flex='column' className='grow gap-3'>
 				<EditAvailability />
+				<EditSocials />
 			</Flex>
 		</Flex>
 	);
