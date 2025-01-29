7e6dc1bc08035f34c5a9ab5795017842ee993d07
davitti <abuadonald@gmail.com>
2025-01-29 17:39:02 +0100

    Replace Skills component with EditTechStack in DeveloperOverview.

--- src/app/(dashboard)/components/developer-overview.tsx
+++ src/app/(dashboard)/components/developer-overview.tsx
@@ -2,14 +2,14 @@ import Flex from "@/components/layouts/flex";
 import React from "react";
 import EditDeveloperSummary from "./edit-developer-summary";
 import Portfolio from "./portfolio";
-import Skills from "./skills";
+import EditTechStack from "./edit-tech-stack";
 
 export default function DeveloperOverview() {
 	return (
 		<Flex className='basis-[720px] grow-[2] gap-3' flex='column'>
 			<EditDeveloperSummary />
 			<Portfolio />
-			<Skills />
+			<EditTechStack />
 		</Flex>
 	);
 }
