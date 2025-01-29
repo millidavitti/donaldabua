16b92af1d2955a0d92adfabf28e94e60c82cb26b
davitti <abuadonald@gmail.com>
2025-01-29 17:39:55 +0100

    Add TechStack component for displaying technology stack.

---
+++ src/app/(dashboard)/components/tech-stack.tsx
@@ -0,0 +1,30 @@
+import Flex from "@/components/layouts/flex";
+import { X } from "lucide-react";
+import React from "react";
+
+export default function TechStack() {
+	return (
+		<Flex className='gap-3 flex-wrap shrink-0'>
+			{techStack.map((tech) => (
+				<Flex className='gap-3 items-center' key={tech}>
+					<p className='shrink-0 font-medium'>{tech}</p>
+					<X
+						size={24}
+						className='stroke-light-error cursor-pointer active:scale-[.95]'
+					/>
+				</Flex>
+			))}
+		</Flex>
+	);
+}
+
+const techStack = [
+	"Next JS",
+	"Express",
+	"Node",
+	"Prisma",
+	"Neo4j",
+	"Typescript",
+	"Github",
+	"Azure",
+];
