1a73ccc2723e96a34599668ceab9c1f666f9001a
davitti <abuadonald@gmail.com>
2025-01-29 12:52:47 +0100

    Refactor EditDeveloperSummary layout to include EditProfileOverview and improve styling.

--- src/app/(dashboard)/components/edit-developer-summary.tsx
+++ src/app/(dashboard)/components/edit-developer-summary.tsx
@@ -2,12 +2,16 @@
 import Flex from "@/components/layouts/flex";
 import EditTitle from "./edit-title";
 import EditHourlyRate from "./edit-hourly-rate";
+import EditProfileOverview from "./edit-profile-overview";
 
 export default function EditDeveloperSummary() {
 	return (
-		<Flex className='basis-40 grow shrink-0 justify-between'>
-			<EditTitle />
-			<EditHourlyRate />
+		<Flex flex='column' className='gap-3'>
+			<Flex className='shrink-0 justify-between flex-wrap gap-3'>
+				<EditTitle />
+				<EditHourlyRate />
+			</Flex>
+			<EditProfileOverview />
 		</Flex>
 	);
 }
