ea65ce217e6e4a80e989ffa2fa1f8785f340eb39
davitti <abuadonald@gmail.com>
2025-02-03 16:21:06 +0100

    Add constants for edit profile and portfolio project states

---
+++ src/data/constants.ts
@@ -0,0 +1,16 @@
+export const EDIT_PROFILE_STATES = [
+	"edit-image",
+	"edit-location",
+	"edit-name",
+	"edit-video",
+	"edit-hours-per-week",
+	"edit-title",
+	"edit-hourly-rate",
+	"edit-profile-overview",
+	"edit-tech-stack",
+	"edit-employment-history",
+	"edit-socials",
+	"edit-portfolio",
+];
+
+export const EDIT_PORTFOLIO_PROJECT_STATES = ["edit-portfolio-project-video"];
