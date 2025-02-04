56b7dd762af80147b2a065e82f11afa2f2eb3cd6
davitti <abuadonald@gmail.com>
2025-02-04 11:38:51 +0100

    Implement markdown editor for portfolio project text.

--- src/app/(dashboard)/components/edit-portfolio-project-text.tsx
+++ src/app/(dashboard)/components/edit-portfolio-project-text.tsx
@@ -2,18 +2,25 @@ import Flex from "@/components/layouts/flex";
 import InteractiveIcon from "@/components/layouts/interactive_icon";
 import Overlay from "@/components/layouts/overlay";
 import Button from "@/components/ui/button";
+import { portfolio_project_data_jotai } from "@/data/atoms/app_data";
 import {
 	content_hover_state_jotai,
 	edit_portfolio_project_jotai,
 } from "@/data/atoms/ui_state";
 import { useSetAtom } from "jotai";
 import { Text, X } from "lucide-react";
-
+import React, { useState } from "react";
+import md from "md";
+import parse from "html-react-parser";
 export default function EditPortfolioProjectText() {
 	const edit_portfolio_project_setter = useSetAtom(
 		edit_portfolio_project_jotai,
 	);
 	const content_hover_state_setter = useSetAtom(content_hover_state_jotai);
+	const [markdown, setMarkdown] = useState("");
+	const portfolio_project_data_setter = useSetAtom(
+		portfolio_project_data_jotai,
+	);
 	return (
 		<>
 			<InteractiveIcon
@@ -47,8 +54,38 @@ export default function EditPortfolioProjectText() {
 					</Flex>
 					{/* Nested Form */}
 					<Flex flex='column' className='gap-3'>
-						<textarea id='title' required rows={10} className='outline p-3' />
-						<Button className='bg-black text-light-surface'>Add</Button>
+						<textarea
+							id='title'
+							required
+							value={markdown}
+							onChange={(e) => {
+								setMarkdown(e.target.value);
+							}}
+							rows={10}
+							className='outline p-3'
+						>
+							{parse(md(markdown))}
+						</textarea>
+						<Button
+							className='bg-black text-light-surface'
+							onClick={() => {
+								portfolio_project_data_setter((data) => ({
+									...data,
+									content: [
+										...data.content,
+										{
+											markdown,
+											position: data.content.length,
+											type: "text",
+										},
+									],
+								}));
+								setMarkdown("");
+								edit_portfolio_project_setter(null);
+							}}
+						>
+							Add
+						</Button>
 					</Flex>
 				</Flex>
 			</Overlay>
