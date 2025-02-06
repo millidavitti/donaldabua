34edad8f54341461e9487a1f852a005a3cbb5f4e
davitti <abuadonald@gmail.com>
2025-02-07 00:55:27 +0100

    Update import path for PreviewPortfolioProjectDraft component.

--- src/app/(dashboard)/components/edit-portfolio.tsx
+++ src/app/(dashboard)/components/edit-portfolio.tsx
@@ -9,7 +9,7 @@ import {
 import { useAtomValue, useSetAtom } from "jotai";
 import { CirclePlus } from "lucide-react";
 import DraftPortfolioProjectInfo from "./draft-portfolio-project-info";
-import PreviewPortfolioProjectDraft from "./preview-portfolio-project-draft";
+import PreviewPortfolioProjectDraft from "./preview-portfolio-project/preview-portfolio-project-draft";
 import ContentBuilder from "./content-builder/content-builder";
 
 export default function EditPortfolio() {
