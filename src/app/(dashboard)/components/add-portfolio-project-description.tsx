029cda3a1f79ac5b25dc3722a39675aa65bbf145
davitti <abuadonald@gmail.com>
2025-02-06 15:12:10 +0100

    Add AddPortfolioProjectDescription component for project descriptions.

---
+++ src/app/(dashboard)/components/add-portfolio-project-description.tsx
@@ -0,0 +1,24 @@
+import { portfolio_project_description_jotai } from "@/data/atoms/app_data";
+import { useAtom } from "jotai";
+import React from "react";
+
+export default function AddPortfolioProjectDescription() {
+	const [portfolio_project_description, portfolio_project_description_setter] =
+		useAtom(portfolio_project_description_jotai);
+	return (
+		<>
+			<label className='text-xl font-semibold shrink-0' htmlFor='title'>
+				Project Description
+			</label>
+			<input
+				type='text'
+				required
+				className='outline p-3'
+				value={portfolio_project_description}
+				onChange={(e) => {
+					portfolio_project_description_setter(e.target.value);
+				}}
+			/>
+		</>
+	);
+}
