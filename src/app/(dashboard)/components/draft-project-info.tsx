f90bec747571f763c4090dfc6ef36deb8d5cf6f5
davitti <abuadonald@gmail.com>
2025-02-06 15:12:41 +0100

    Refactor DraftProjectInfo component to use separate subcomponents for title, description, tech stack, and thumbnail.

--- src/app/(dashboard)/components/draft-project-info.tsx
+++ src/app/(dashboard)/components/draft-project-info.tsx
@@ -15,6 +15,10 @@ import {
 	portfolio_project_title_jotai,
 } from "@/data/atoms/app_data";
 import Image from "next/image";
+import AddPortfolioProjectTitle from "./add-portfolio-project-title";
+import AddPortfolioProjectDescription from "./add-portfolio-project-description";
+import AddPortfolioProjectTechStack from "./add-portfolio-project-tech-stack";
+import AddPortfolioProjectThumbnail from "./add-portfolio-project-thumbnail";
 
 interface DraftProjectInfo {
 	children: ReactNode;
@@ -24,16 +28,6 @@ export default function DraftProjectInfo({ children }: DraftProjectInfo) {
 	const portfolio_project_form_step_setter = useSetAtom(
 		portfolio_project_form_step_jotai,
 	);
-
-	const [portfolio_project_tech_stack, portfolio_project_tech_stack_setter] =
-		useAtom(portfolio_project_tech_stack_jotai);
-	const [portfolio_project_description, portfolio_project_description_setter] =
-		useAtom(portfolio_project_description_jotai);
-	const [portfolio_project_title, portfolio_project_title_setter] = useAtom(
-		portfolio_project_title_jotai,
-	);
-	const [portfolio_project_thumbnail, portfolio_project_thumbnail_setter] =
-		useAtom(portfolio_project_thumbnail_jotai);
 
 	return (
 		<Flex
@@ -55,91 +49,17 @@ export default function DraftProjectInfo({ children }: DraftProjectInfo) {
 					edit_profile_setter(null);
 				}}
 			>
-				{/* Project Title */}
-				<label className='text-xl font-semibold shrink-0' htmlFor='title'>
-					Project Title
-				</label>
-				<input
-					type='text'
-					required
-					className='outline p-3 shrink-0'
-					value={portfolio_project_title}
-					onChange={(e) => {
-						portfolio_project_title_setter(e.target.value);
-					}}
-				/>
+				<AddPortfolioProjectTitle />
 
 				{/* Project Description, Tech Stack, Content */}
 				<Flex className='gap-3 flex-wrap'>
 					{/* Project Description, Tech Stack */}
 					<Flex flex='column' className='grow gap-3 basis-[360px] h-fit'>
-						{/* Project Description */}
-						<label className='text-xl font-semibold shrink-0' htmlFor='title'>
-							Project Description
-						</label>
-						<input
-							type='text'
-							required
-							className='outline p-3'
-							value={portfolio_project_description}
-							onChange={(e) => {
-								portfolio_project_description_setter(e.target.value);
-							}}
-						/>
-
+						<AddPortfolioProjectDescription />
 						{/* Tech Stack */}
-						<label className='text-xl font-semibold shrink-0' htmlFor='title'>
-							Tech Stack
-						</label>
-						<Flex className='gap-3 flex-wrap shrink-0'>
-							{portfolio_project_tech_stack.map((tech) => (
-								<Flex className='gap-3 items-center' key={tech}>
-									<p className='shrink-0 font-medium'>{tech}</p>
-									<X
-										size={24}
-										className='stroke-light-error cursor-pointer active:scale-[.95]'
-									/>
-								</Flex>
-							))}
-						</Flex>
-						<input
-							type='text'
-							required
-							className='outline p-3'
-							onKeyDown={(e) => {
-								if (e.key === "Enter") {
-									if ((e.target as HTMLInputElement).value)
-										portfolio_project_tech_stack_setter((stack) => [
-											...stack,
-											(e.target as HTMLInputElement).value,
-										]);
-									(e.target as HTMLInputElement).value = "";
-								}
-							}}
-						/>
+						<AddPortfolioProjectTechStack />
 						{/* Thumbnail */}
-						<label className='text-xl font-semibold shrink-0' htmlFor='title'>
-							Thumbnail
-						</label>
-						<input
-							type='url'
-							required
-							className='outline p-3'
-							value={portfolio_project_thumbnail}
-							onChange={(e) => {
-								portfolio_project_thumbnail_setter(e.target.value);
-							}}
-						/>
-						{portfolio_project_thumbnail && (
-							<Image
-								src={portfolio_project_thumbnail}
-								width={1000}
-								height={1000}
-								alt='thumbnail'
-								data-is-visible={Boolean(portfolio_project_thumbnail)}
-								className='data-[is-visible=false]:hidden aspect-[16/9] outline-2 outline neonScan'
-							/>
-						)}
+						<AddPortfolioProjectThumbnail />
 					</Flex>
 
 					{children}
