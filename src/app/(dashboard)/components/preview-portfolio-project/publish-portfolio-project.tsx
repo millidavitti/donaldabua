62a909068e573dc60d7461fcbbcc85ac63a52180
davitti <abuadonald@gmail.com>
2025-02-07 14:53:49 +0100

    Refactor PublishPortfolioProject to use custom hook for publishing action.

--- src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
+++ src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
@@ -1,25 +1,17 @@
 import Button from "@/components/ui/button";
 import { portfolio_project_data_jotai } from "@/data/atoms/app_data";
 import { mock_portfolio_projects_jotai } from "@/data/mock";
+import usePublishPortfolioProjectInterface from "@/hooks/interface/use-publish-portfolio-project-interface";
 import { useAtom, useSetAtom } from "jotai";
 import React from "react";
 
 export default function PublishPortfolioProject() {
-	const [portfolio_project_data] = useAtom(portfolio_project_data_jotai);
-	const mock_portfolio_projects_setter = useSetAtom(
-		mock_portfolio_projects_jotai,
-	);
+	const { publishPortfolioProject } = usePublishPortfolioProjectInterface();
 	return (
 		<Button
 			type='button'
 			className='bg-black text-light-surface'
-			onClick={() => {
-				mock_portfolio_projects_setter((projects) => [
-					...projects,
-					portfolio_project_data,
-				]);
-				// portfolio_project_data_setter({});
-			}}
+			onClick={() => publishPortfolioProject()}
 		>
 			Publish
 		</Button>
