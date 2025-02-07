1116dd3a49583d2c06a4d0d89b2f190ff34d58b0
davitti <abuadonald@gmail.com>
2025-02-07 13:09:55 +0100

    Refactor publish portfolio project component to use atom setters for state management.

--- src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
+++ src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
@@ -1,16 +1,26 @@
 import Button from "@/components/ui/button";
 import { portfolio_project_data_jotai } from "@/data/atoms/app_data";
-import { useAtomValue } from "jotai";
+import { mock_portfolio_projects_jotai } from "@/data/mock";
+import { useAtom, useSetAtom } from "jotai";
 import React from "react";
 
 export default function PublishPortfolioProject() {
-	const portfolio_project_data = useAtomValue(portfolio_project_data_jotai);
+	const [portfolio_project_data, portfolio_project_data_setter] = useAtom(
+		portfolio_project_data_jotai,
+	);
+	const mock_portfolio_projects_setter = useSetAtom(
+		mock_portfolio_projects_jotai,
+	);
 	return (
 		<Button
 			type='button'
 			className='bg-black text-light-surface'
 			onClick={() => {
-				console.log(portfolio_project_data);
+				mock_portfolio_projects_setter((projects) => [
+					...projects,
+					portfolio_project_data,
+				]);
+				portfolio_project_data_setter({});
 			}}
 		>
 			Publish
