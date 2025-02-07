7a69be69b06f6c1a1ee9b8e355b0b7e1d554622c
davitti <abuadonald@gmail.com>
2025-02-08 00:14:28 +0100

    Add PublishedPortfolioProjectTechStack component.

---
+++ src/app/(dashboard)/components/published-portfolio-project/published-portfolio-project-tech-stack.tsx
@@ -0,0 +1,29 @@
+import Flex from "@/components/layouts/flex";
+import { X } from "lucide-react";
+import React from "react";
+
+export default function PublishedPortfolioProjectTechStack({
+	techStack,
+}: {
+	techStack: string[];
+}) {
+	return (
+		<>
+			{/* Tech Stack */}
+			<label className='text-xl font-semibold shrink-0' htmlFor='title'>
+				Tech Stack
+			</label>
+			<Flex className='gap-3 flex-wrap shrink-0'>
+				{techStack.map((tech) => (
+					<Flex className='gap-3 items-center' key={tech}>
+						<p className='shrink-0 font-medium'>{tech}</p>
+						<X
+							size={24}
+							className='stroke-light-error cursor-pointer active:scale-[.95]'
+						/>
+					</Flex>
+				))}
+			</Flex>
+		</>
+	);
+}
