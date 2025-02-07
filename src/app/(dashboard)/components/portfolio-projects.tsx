d1d594071d675d252600edfb0b9723655fe08d70
davitti <abuadonald@gmail.com>
2025-02-08 00:14:13 +0100

    Refactor PortfolioProjects component to use project object and improve structure.

--- src/app/(dashboard)/components/portfolio-projects.tsx
+++ src/app/(dashboard)/components/portfolio-projects.tsx
@@ -3,20 +3,18 @@ import React from "react";
 import PortfolioProject from "./portfolio-project";
 import { mock_portfolio_projects_jotai } from "@/data/mock";
 import { useAtomValue } from "jotai";
+import Overlay from "@/components/layouts/overlay";
+import ViewPortfolioProject from "./view-portfolio-project";
 
 export default function PortfolioProjects() {
 	const mock_portfolio_projects = useAtomValue(mock_portfolio_projects_jotai);
 	return (
-		<Flex className='flex-wrap gap-3 max-h-[484px]'>
-			{mock_portfolio_projects.map((project) => {
-				return (
-					<PortfolioProject
-						key={project.id}
-						thumbnail={project.thumbnail}
-						title={project.title}
-					/>
-				);
-			})}
-		</Flex>
+		<>
+			<Flex className='flex-wrap gap-3 max-h-[484px]'>
+				{mock_portfolio_projects.map((project) => {
+					return <PortfolioProject key={project.id} project={project} />;
+				})}
+			</Flex>
+		</>
 	);
 }
