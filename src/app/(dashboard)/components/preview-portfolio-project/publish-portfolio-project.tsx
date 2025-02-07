4b739bd6996aa8f0633088e3ce9bd9ac046a3a0c
davitti <abuadonald@gmail.com>
2025-02-07 15:01:26 +0100

    Remove unused imports in PublishPortfolioProject component.

--- src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
+++ src/app/(dashboard)/components/preview-portfolio-project/publish-portfolio-project.tsx
@@ -1,8 +1,5 @@
 import Button from "@/components/ui/button";
-import { portfolio_project_data_jotai } from "@/data/atoms/app_data";
-import { mock_portfolio_projects_jotai } from "@/data/mock";
 import usePublishPortfolioProjectInterface from "@/hooks/interface/use-publish-portfolio-project-interface";
-import { useAtom, useSetAtom } from "jotai";
 import React from "react";
 
 export default function PublishPortfolioProject() {
