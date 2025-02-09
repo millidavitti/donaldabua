c51f0481d23b52c86309d0b764b3dd141a030b47
davitti <abuadonald@gmail.com>
2025-02-09 15:55:26 +0100

    Rename get-user-action.ts to get-user-controller.ts and update user media links.

--- src/backend/get-user-controller.ts
+++ src/backend/get-user-controller.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { ProfileUser } from "@/data/atoms/app_data";
 import { createId } from "@paralleldrive/cuid2";
 
@@ -7,8 +5,9 @@ export async function getUser() {
 	return {
 		id: createId(),
 		name: "Donald Abua",
-		image: "/stud.jpg",
-		video: "https://youtu.be/HLTin4qpMD8?si=11FGMPwaZ6M3LDYb",
+		image:
+			"https://res.cloudinary.com/torch-cms-media/image/upload/v1657611196/god_of_war_2018_video_game_wallpaper_1366x768_0ab2e9648c.jpg",
+		video: "https://youtube.com/embed/-2k1rcRzsLA",
 		location: {
 			city: "New York",
 			country: "United State",
