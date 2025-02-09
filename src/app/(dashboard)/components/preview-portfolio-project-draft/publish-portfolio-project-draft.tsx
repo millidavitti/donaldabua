95bc41d2e6f8d3d67991190e43f7cf8cf24f4fbb
davitti <abuadonald@gmail.com>
2025-02-09 01:22:48 +0100

    Refactor publish button logic to handle save and publish states.

--- src/app/(dashboard)/components/preview-portfolio-project-draft/publish-portfolio-project-draft.tsx
+++ src/app/(dashboard)/components/preview-portfolio-project-draft/publish-portfolio-project-draft.tsx
@@ -3,14 +3,24 @@ import usePublishPortfolioProjectInterface from "@/hooks/interface/use-publish-p
 import React from "react";
 
 export default function PublishPortfolioProject() {
-	const { publishPortfolioProject } = usePublishPortfolioProjectInterface();
+	const {
+		publishPortfolioProject,
+		editProfileState,
+		savePublishedPortfolioProjectEdit,
+	} = usePublishPortfolioProjectInterface();
 	return (
 		<Button
 			type='button'
 			className='bg-black text-light-surface'
-			onClick={() => publishPortfolioProject()}
+			onClick={() =>
+				editProfileState === "edit-published-portfolio-project"
+					? savePublishedPortfolioProjectEdit()
+					: publishPortfolioProject()
+			}
 		>
-			Publish
+			{editProfileState === "edit-published-portfolio-project"
+				? "Save"
+				: "Publish"}
 		</Button>
 	);
 }
