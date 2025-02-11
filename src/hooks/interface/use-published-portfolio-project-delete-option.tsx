81a9bfd0e632ea5c443ebd66b16f01bc8452b12f
davitti <abuadonald@gmail.com>
2025-02-11 14:46:23 +0100

    Refactor to use updated atom for portfolio projects.

--- src/hooks/interface/use-published-portfolio-project-delete-option.tsx
+++ src/hooks/interface/use-published-portfolio-project-delete-option.tsx
@@ -1,12 +1,10 @@
-import { mock_portfolio_projects_jotai } from "@/data/mock";
+import { portfolio_projects_jotai } from "@/data/atoms/app_data";
 import { useSetAtom } from "jotai";
 
 export default function usePublishedPortfolioProjectDeleteOption() {
-	const mock_portfolio_projects_setter = useSetAtom(
-		mock_portfolio_projects_jotai,
-	);
+	const portfolio_projects_setter = useSetAtom(portfolio_projects_jotai);
 	function deleteProject(projectID: string) {
-		mock_portfolio_projects_setter((projects) => {
+		portfolio_projects_setter((projects) => {
 			return projects.filter((project) => project.id !== projectID);
 		});
 	}
