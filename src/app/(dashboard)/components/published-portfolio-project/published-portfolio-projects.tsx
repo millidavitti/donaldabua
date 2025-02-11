127ad061efce01cd1eb9bbcfaf7774d3b56f1400
davitti <abuadonald@gmail.com>
2025-02-11 14:45:21 +0100

    Refactor published portfolio projects to use updated atom values.

--- src/app/(dashboard)/components/published-portfolio-project/published-portfolio-projects.tsx
+++ src/app/(dashboard)/components/published-portfolio-project/published-portfolio-projects.tsx
@@ -1,10 +1,12 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
-import { mock_portfolio_projects_jotai } from "@/data/mock";
 import { useAtomValue } from "jotai";
 import Overlay from "@/components/layouts/overlay";
 import ViewPortfolioProject from "../view-portfolio-project";
-import { selected_portfolio_project_jotai } from "@/data/atoms/app_data";
+import {
+	portfolio_projects_jotai,
+	selected_portfolio_project_jotai,
+} from "@/data/atoms/app_data";
 import PublishedPortfolioProject from "./published-portfolio-project";
 import DraftPortfolioProjectInfo from "../add-portfolio-project/draft-portfolio-project-info";
 import ContentBuilder from "../content-builder/content-builder";
@@ -12,7 +14,7 @@ import PreviewPortfolioProjectDraft from "../preview-portfolio-project-draft/pre
 import { portfolio_project_form_step_jotai } from "@/data/atoms/ui_state";
 
 export default function PublishedPortfolioProjects() {
-	const mock_portfolio_projects = useAtomValue(mock_portfolio_projects_jotai);
+	const portfolio_projects = useAtomValue(portfolio_projects_jotai);
 	const selected_portfolio_project = useAtomValue(
 		selected_portfolio_project_jotai,
 	);
@@ -22,7 +24,7 @@ export default function PublishedPortfolioProjects() {
 	return (
 		<>
 			<Flex className='flex-wrap gap-3 max-h-[484px]'>
-				{mock_portfolio_projects.map((project) => {
+				{portfolio_projects.map((project) => {
 					return (
 						<PublishedPortfolioProject key={project.id} project={project} />
 					);
