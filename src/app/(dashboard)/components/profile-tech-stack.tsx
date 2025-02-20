59be228de28d90be11f999b21acabeb225e3c753
davitti <abuadonald@gmail.com>
2025-02-20 23:32:43 +0100

    Add ProfileTechStack component to display technology stack

---
+++ src/app/(dashboard)/components/profile-tech-stack.tsx
@@ -0,0 +1,46 @@
+import Flex from "@/components/layouts/flex";
+import { X } from "lucide-react";
+import React from "react";
+
+export default function ProfileTechStack() {
+	return (
+		<Flex className='gap-3 flex-wrap shrink-0 grow'>
+			{techStack.map((tech) => (
+				<Flex className='gap-3 items-center' key={tech.id}>
+					<p className='shrink-0 font-medium'>{tech.name}</p>
+					<X
+						size={24}
+						className='stroke-light-error cursor-pointer active:scale-[.95]'
+					/>
+				</Flex>
+			))}
+		</Flex>
+	);
+}
+
+const techStack = [
+	{ id: 1, name: "Next JS" },
+	{ id: 2, name: "Express" },
+	{ id: 3, name: "Node" },
+	{ id: 4, name: "Prisma" },
+	{ id: 5, name: "Neo4j" },
+	{ id: 6, name: "Typescript" },
+	{ id: 7, name: "Github" },
+	{ id: 8, name: "Azure" },
+];
+
+/**
+ * Portfolio Project Tech Stack
+ * Composite Key:
+ * name + projectId or techId + projectId
+ */
+
+/**
+ * id | name |
+ * 1 | Next JS
+ * 2 | Express
+ * 3 | Node
+ * 4 | Prisma
+ * 5 | Neo4j
+ *
+ */
