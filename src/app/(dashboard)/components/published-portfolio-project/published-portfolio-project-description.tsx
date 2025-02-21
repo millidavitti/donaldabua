460b942363509b0bbf1c1992b87317bf7cea743b
davitti <abuadonald@gmail.com>
2025-02-21 14:39:37 +0100

    Refactor PublishedPortfolioProjectDescription component to update naming for improved clarity and consistency

--- src/app/(dashboard)/components/published-portfolio-project/published-portfolio-project-description.tsx
+++ src/app/(dashboard)/components/published-portfolio-project/published-portfolio-project-description.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function PublishedPortfolioProjectDescription({
+export default function PublishedProjectDescription({
 	description,
 }: {
 	description: string;
