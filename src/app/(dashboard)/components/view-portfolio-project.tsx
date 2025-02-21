be4d8f256d5ba63d7aa35a32bfebfaa1ea5dd94f
davitti <abuadonald@gmail.com>
2025-02-21 14:39:24 +0100

    Refactor ViewPortfolioProject component to update naming conventions for improved clarity and consistency

--- src/app/(dashboard)/components/view-portfolio-project.tsx
+++ src/app/(dashboard)/components/view-portfolio-project.tsx
@@ -2,11 +2,11 @@ import Flex from "@/components/layouts/flex";
 import InteractiveIcon from "@/components/layouts/interactive_icon";
 import { ArrowLeftIcon } from "lucide-react";
 import React from "react";
-import PublishedPortfolioProjectTitle from "./published-portfolio-project/published-portfolio-project-title";
-import PublishedPortfolioProjectDescription from "./published-portfolio-project/published-portfolio-project-description";
-import PublishedPortfolioProjectTechStack from "./published-portfolio-project/published-portfolio-project-tech-stack";
-import PublishedPortfolioProjectThumbnail from "./published-portfolio-project/published-portfolio-project-thumbnail";
-import PublishedPortfolioProjectContent from "./published-portfolio-project/published-portfolio-project-content";
+import PublishedProjectTitle from "./published-portfolio-project/published-portfolio-project-title";
+import PublishedProjectDescription from "./published-portfolio-project/published-portfolio-project-description";
+import PublishedProjectTechStack from "./published-portfolio-project/published-portfolio-project-tech-stack";
+import PublishedProjectThumbnail from "./published-portfolio-project/published-portfolio-project-thumbnail";
+import PublishedProjectContent from "./published-portfolio-project/published-portfolio-project-content";
 import { Project } from "@/data/atoms/app_data";
 import { useSetAtom } from "jotai";
 import { edit_profile_jotai } from "@/data/atoms/ui_state";
@@ -37,17 +37,15 @@ export default function ViewProject({ project }: ViewPortfolioProject) {
 
 			<Flex flex='column' className='gap-3'>
 				{/* Project Title */}
-				<PublishedPortfolioProjectTitle title={project.title} />
+				<PublishedProjectTitle title={project.title} />
 
 				<Flex className='gap-3 flex-wrap'>
 					<Flex flex='column' className='grow gap-3 basis-[360px]'>
-						<PublishedPortfolioProjectDescription
-							description={project.description}
-						/>
-						<PublishedPortfolioProjectTechStack techStack={project.techStack} />
-						<PublishedPortfolioProjectThumbnail thumbnail={project.thumbnail} />
+						<PublishedProjectDescription description={project.description} />
+						<PublishedProjectTechStack techStack={project.techStack} />
+						<PublishedProjectThumbnail thumbnail={project.thumbnail} />
 					</Flex>
-					<PublishedPortfolioProjectContent />
+					<PublishedProjectContent />
 				</Flex>
 			</Flex>
 		</Flex>
