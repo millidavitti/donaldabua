e001276ea33363cb654943b5c0fff31ffa6f7984
davitti <abuadonald@gmail.com>
2025-02-21 14:40:13 +0100

    Refactor PublishedPortfolioProjectTitle component to update naming for improved clarity and consistency

--- src/app/(dashboard)/components/published-portfolio-project/published-portfolio-project-title.tsx
+++ src/app/(dashboard)/components/published-portfolio-project/published-portfolio-project-title.tsx
@@ -1,10 +1,6 @@
 import Flex from "@/components/layouts/flex";
 
-export default function PublishedPortfolioProjectTitle({
-	title,
-}: {
-	title: string;
-}) {
+export default function PublishedProjectTitle({ title }: { title: string }) {
 	return (
 		<Flex flex='column' className='shrink-0 gap-3'>
 			<label className='text-xl font-semibold' htmlFor='title'>
