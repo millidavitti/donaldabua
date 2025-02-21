be11aedf9297d0635c8b750722462a0f29108414
davitti <abuadonald@gmail.com>
2025-02-21 14:15:45 +0100

    Refactor ContentBuilder component to use project_content_jotai for improved clarity and consistency

--- src/app/(dashboard)/components/content-builder/content-builder.tsx
+++ src/app/(dashboard)/components/content-builder/content-builder.tsx
@@ -1,7 +1,7 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
 import ProjectContentOptions from "../add-portfolio-project/portfolio-project-content-options";
-import { portfolio_project_content_jotai } from "@/data/atoms/app_data";
+import { project_content_jotai } from "@/data/atoms/app_data";
 import { useAtomValue } from "jotai";
 import ContentBuilderImage from "./content-builder-image";
 import ContentBuilderVideo from "./content-builder-video";
@@ -9,16 +9,14 @@ import ContentBuilderText from "./content-builder-text";
 import { createId } from "@paralleldrive/cuid2";
 
 export default function ContentBuilder() {
-	const portfolio_project_content = useAtomValue(
-		portfolio_project_content_jotai,
-	);
-	console.log(portfolio_project_content);
+	const project_content = useAtomValue(project_content_jotai);
+
 	return (
 		<Flex flex='column' className='basis-[360px] grow-[2] gap-3'>
 			<ProjectContentOptions />
-			{Boolean(portfolio_project_content.length) && (
+			{Boolean(project_content.length) && (
 				<Flex flex='column' className='h-fit gap-3'>
-					{portfolio_project_content
+					{project_content
 						.sort((a, b) => a.position - b.position)
 						.map((component) => {
 							if (component.type === "image")
