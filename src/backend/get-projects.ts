25ccb5f54b90e55f62ce867c53ea3974f3b0561a
davitti <abuadonald@gmail.com>
2025-02-21 14:42:37 +0100

    Add getProjects function to fetch projects by profile ID

---
+++ src/backend/get-projects.ts
@@ -0,0 +1,15 @@
+import { Project } from "@/data/atoms/app_data";
+import { createId } from "@paralleldrive/cuid2";
+
+export async function getProjects(profileId: string) {
+	try {
+		const res = await fetch(
+			process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "/projects/" + profileId,
+		);
+		const { projects } = await res.json();
+
+		return projects as Project[];
+	} catch (error) {
+		console.log("---getProjects---\n", error);
+	}
+}
