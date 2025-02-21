4970fb7d62a0483941a0d4a271d22a25bb666a11
davitti <abuadonald@gmail.com>
2025-02-21 14:54:29 +0100

    Refactor portfolio project interface hooks for improved naming consistency

--- src/hooks/interface/use-publish-portfolio-project-interface.tsx
+++ src/hooks/interface/use-publish-portfolio-project-interface.tsx
@@ -1,65 +1,59 @@
 import {
 	defaultStore,
-	portfolio_project_content_jotai,
-	portfolio_project_description_jotai,
-	portfolio_project_tech_stack_jotai,
-	portfolio_project_thumbnail_jotai,
-	portfolio_project_title_jotai,
-	portfolio_projects_jotai,
+	project_content_jotai,
+	project_description_jotai,
+	project_technologies_jotai,
+	project_thumbnail_jotai,
+	project_title_jotai,
+	projects_jotai,
 } from "@/data/atoms/app_data";
 import {
 	edit_profile_jotai,
-	portfolio_project_to_edit_jotai,
+	project_to_edit_jotai,
 } from "@/data/atoms/ui_state";
 import { createId } from "@paralleldrive/cuid2";
 import { useAtomValue, useSetAtom } from "jotai";
-import useResetPortfolioProjectFormFields from "../use-reset-portfolio-project-form-fields";
+import useResetProjectFormFields from "../use-reset-portfolio-project-form-fields";
 
 export default function usePublishPortfolioProjectInterface() {
-	const portfolio_projects_setter = useSetAtom(portfolio_projects_jotai);
-	const portfolio_project_to_edit = useAtomValue(
-		portfolio_project_to_edit_jotai,
-	);
+	const projects_setter = useSetAtom(projects_jotai);
+	const project_to_edit = useAtomValue(project_to_edit_jotai);
 	const edit_profile = useAtomValue(edit_profile_jotai);
-	const resetPortfolioProjectFormFields = useResetPortfolioProjectFormFields();
+	const resetProjectFormFields = useResetProjectFormFields();
 
-	function publishPortfolioProject() {
-		portfolio_projects_setter((projects) => [
+	function publishProject() {
+		projects_setter((projects) => [
 			...projects,
 			{
 				id: createId(),
-				content: defaultStore.get(portfolio_project_content_jotai),
-				description: defaultStore.get(portfolio_project_description_jotai),
-				techStack: defaultStore.get(portfolio_project_tech_stack_jotai),
-				thumbnail: defaultStore.get(portfolio_project_thumbnail_jotai),
-				title: defaultStore.get(portfolio_project_title_jotai),
+				description: defaultStore.get(project_description_jotai),
+				thumbnail: defaultStore.get(project_thumbnail_jotai),
+				title: defaultStore.get(project_title_jotai),
 			},
 		]);
 
-		resetPortfolioProjectFormFields();
+		resetProjectFormFields();
 	}
 
-	function savePublishedPortfolioProjectEdit() {
-		portfolio_projects_setter((projects) =>
+	function savePublishedProjectEdit() {
+		projects_setter((projects) =>
 			projects.map((project) => {
-				if (portfolio_project_to_edit! === project.id)
+				if (project_to_edit! === project.id)
 					return {
 						id: createId(),
-						content: defaultStore.get(portfolio_project_content_jotai),
-						description: defaultStore.get(portfolio_project_description_jotai),
-						techStack: defaultStore.get(portfolio_project_tech_stack_jotai),
-						thumbnail: defaultStore.get(portfolio_project_thumbnail_jotai),
-						title: defaultStore.get(portfolio_project_title_jotai),
+						description: defaultStore.get(project_description_jotai),
+						thumbnail: defaultStore.get(project_thumbnail_jotai),
+						title: defaultStore.get(project_title_jotai),
 					};
 				return project;
 			}),
 		);
-		resetPortfolioProjectFormFields();
+		resetProjectFormFields();
 	}
 
 	return {
-		publishPortfolioProject,
-		savePublishedPortfolioProjectEdit,
+		publishProject,
+		savePublishedProjectEdit,
 		editProfileState: edit_profile,
 	};
 }
