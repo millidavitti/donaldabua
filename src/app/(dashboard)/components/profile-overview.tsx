e711fe60c33fe59599847677013eeee95fd1087e
davitti <abuadonald@gmail.com>
2025-02-21 16:32:38 +0100

    Update import path for EditProfileTechnologies component in ProfileOverview

--- src/app/(dashboard)/components/profile-overview.tsx
+++ src/app/(dashboard)/components/profile-overview.tsx
@@ -1,7 +1,7 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
 import EditProjects from "./edit-profile/edit-profile-portfolio";
-import EditProfileTechnologies from "./edit-profile/edit-profile-tech-stack";
+import EditProfileTechnologies from "./edit-profile/edit-profile-technologies";
 import EditProfileSummary from "./edit-profile/edit-profile-summary";
 
 export default function ProfileOverview() {
