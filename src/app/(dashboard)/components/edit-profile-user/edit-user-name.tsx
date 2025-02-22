87cf95381ab564a5d7f3edcd5c5bc59ed3c6fc1d
davitti <abuadonald@gmail.com>
2025-02-22 13:14:55 +0100

    Refactor EditProfileUserName component to EditUserName and enhance name editing functionality

--- src/app/(dashboard)/components/edit-profile-user/edit-user-name.tsx
+++ src/app/(dashboard)/components/edit-profile-user/edit-user-name.tsx
@@ -1,49 +1,74 @@
+import Flex from "@/components/layouts/flex";
 import InteractiveIcon from "@/components/layouts/interactive_icon";
-import { user_name_jotai } from "@/data/atoms/app_data";
-import { edit_profile_jotai } from "@/data/atoms/ui_state";
-import { useAtom } from "jotai";
+import Overlay from "@/components/layouts/overlay";
+import Button from "@/components/ui/button";
+import { userEditUserNameInterface } from "@/hooks/interface/user-edit-user-name-interface";
 import { X } from "lucide-react";
-import React from "react";
 
-export default function EditProfileUserName() {
-	const [edit_profile, edit_profile_setter] = useAtom(edit_profile_jotai);
-	const [user_name, user_name_setter] = useAtom(user_name_jotai);
-
+export default function EditUserName() {
+	const {
+		cancelNameEdit,
+		captureNameEdit,
+		edit_profile,
+		saveNameEdit,
+		user_name,
+		editName,
+	} = userEditUserNameInterface();
 	return (
 		<>
 			<h2
 				className='font-bold text-4xl cursor-pointer data-[is-visible=false]:absolute data-[is-visible=true]:hidden'
 				onClick={() => {
-					edit_profile_setter(
-						edit_profile === "edit-name" ? null : "edit-name",
-					);
+					editName();
 				}}
 			>
 				{user_name}
 			</h2>
-			{edit_profile === "edit-name" && (
-				<form
-					className='flex outline bg-light-surface data-[is-visible=true]:absolute data-[is-visible=false]:hidden'
-					data-is-visible={edit_profile === "edit-name"}
-					onSubmit={(e) => {
-						e.preventDefault();
-						edit_profile_setter(null);
-					}}
+			<Overlay
+				stateFlag='edit-name'
+				className='flex justify-center items-center'
+			>
+				<Flex
+					flex='column'
+					className='bg-light-surface gap-3 neonScan max-w-[480px] w-full'
 				>
-					<input
-						type='text'
-						required
-						className='outline-none p-3'
-						value={user_name}
-						onChange={(e) => {
-							user_name_setter(e.target.value);
+					<Flex className='justify-between items-center'>
+						<h2 className='text-2xl font-semibold'>Name</h2>
+						<InteractiveIcon
+							callback={() => {
+								cancelNameEdit();
+							}}
+						>
+							<X size={24} className='stroke-light-error' />
+						</InteractiveIcon>
+					</Flex>
+
+					<form
+						className='flex flex-col gap-3 bg-light-surface data-[is-visible=false]:hidden'
+						data-is-visible={edit_profile === "edit-name"}
+						onSubmit={(e) => {
+							e.preventDefault();
+							saveNameEdit();
 						}}
-					/>
-					<InteractiveIcon callback={() => edit_profile_setter(null)}>
-						<X className='stroke-light-error' />
-					</InteractiveIcon>
-				</form>
-			)}
+					>
+						<label className='text-lg font-semibold' htmlFor='title'>
+							A name you want to be address with
+						</label>
+						<input
+							type='text'
+							required
+							className='outline p-3'
+							value={user_name}
+							onChange={(e) => {
+								captureNameEdit(e.target.value);
+							}}
+						/>
+						<Button type='submit' className='bg-black text-light-surface'>
+							Save
+						</Button>
+					</form>
+				</Flex>
+			</Overlay>
 		</>
 	);
 }
