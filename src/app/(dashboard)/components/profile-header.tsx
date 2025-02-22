c4a173689a8838e86481b31e4582c25acd3497be
davitti <abuadonald@gmail.com>
2025-02-22 01:13:07 +0100

    Rename EditProfileUserPhoto import to EditUserPhoto for consistency

--- src/app/(dashboard)/components/profile-header.tsx
+++ src/app/(dashboard)/components/profile-header.tsx
@@ -1,14 +1,14 @@
 "use client";
 import Flex from "@/components/layouts/flex";
 import React from "react";
-import EditProfileUserPhoto from "./edit-profile-user/edit-profile-user-photo";
+import EditUserPhoto from "./edit-profile-user/edit-user-photo";
 import EditProfileUserName from "./edit-profile-user/edit-profile-user-name";
 import EditProfileUserLocation from "./edit-profile-user/edit-profile-user-location";
 
 export default function ProfileHeader() {
 	return (
 		<Flex className='shrink-0 gap-3 overflow-visible flex-wrap'>
-			<EditProfileUserPhoto />
+			<EditUserPhoto />
 			<Flex flex='column' className='relative overflow-visible gap-3 grow'>
 				<EditProfileUserName />
 				<EditProfileUserLocation />
