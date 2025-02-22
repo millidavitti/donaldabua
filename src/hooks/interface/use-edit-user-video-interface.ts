f009945f46b488bb66a02bfd989544c34d4650dc
davitti <abuadonald@gmail.com>
2025-02-22 16:51:15 +0100

    Add custom hook for editing user videos with validation and error handling

---
+++ src/hooks/interface/use-edit-user-video-interface.ts
@@ -0,0 +1,39 @@
+import { updateUserController } from "@/backend/update-user.controller";
+import { user_jotai, user_video_jotai } from "@/data/atoms/app_data";
+import { edit_profile_jotai } from "@/data/atoms/ui_state";
+import { validateAndEmbedYouTubeUrl } from "@/utils/validate-and-embed-youtube-url";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { toast } from "sonner";
+
+export default function useEditUserVideoInterface() {
+	const edit_profile_setter = useSetAtom(edit_profile_jotai);
+	const [user_video, user_video_setter] = useAtom(user_video_jotai);
+	const { id: userId, video } = useAtomValue(user_jotai);
+
+	function cancelVideoEdit() {
+		user_video_setter(video || "");
+		edit_profile_setter(null);
+	}
+
+	async function saveVideoEdit() {
+		try {
+			edit_profile_setter(null);
+			const { error } = await updateUserController(userId, {
+				video: user_video,
+			});
+
+			if (error) toast.error("Update failed. Please try again later");
+		} catch (error) {
+			user_video_setter(video || "");
+			toast.error("Update failed. Please try again later");
+			console.log("---saveVideoEdit---\n", error);
+		}
+	}
+
+	function captureVideoEdit(value: string) {
+		const youtubeEmbed = validateAndEmbedYouTubeUrl(value);
+		if (youtubeEmbed || !value) user_video_setter(youtubeEmbed || null);
+		else toast.info("Provide a valid YouTube link: " + value);
+	}
+	return { cancelVideoEdit, saveVideoEdit, captureVideoEdit, user_video };
+}
