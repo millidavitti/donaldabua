7da75d58fcf2b7219dc62547cd5623a5b95ddf74
davitti <abuadonald@gmail.com>
2025-02-22 16:50:26 +0100

    Replace EditProfileUserVideoIntroduction with EditUserVideo in Sidebar component

--- src/app/(dashboard)/components/sidebar.tsx
+++ src/app/(dashboard)/components/sidebar.tsx
@@ -2,14 +2,14 @@
 import Flex from "@/components/layouts/flex";
 
 import React from "react";
-import EditProfileUserVideoIntroduction from "./edit-profile-user/edit-profile-user-video-introduction";
+import EditUserVideo from "./edit-profile-user/edit-user-video";
 import EditProfileUserSocials from "./edit-profile-user/edit-profile-user-socials";
 import EditProfileUserAvailability from "./edit-profile/edit-profile-availability";
 
 export default function Sidebar() {
 	return (
 		<Flex flex='column' className='grow gap-3 lg:shrink-0 lg:basis-80'>
-			<EditProfileUserVideoIntroduction />
+			<EditUserVideo />
 			<Flex flex='column' className='grow gap-3'>
 				<EditProfileUserAvailability />
 				<EditProfileUserSocials />
