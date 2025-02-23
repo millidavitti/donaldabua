47e2b04f2052a301071d34794a968f390ff9fda3
davitti <abuadonald@gmail.com>
2025-02-23 22:42:53 +0100

    Add error handling and user state restoration in useEditUserNameInterface

--- src/hooks/interface/use-edit-user-name-interface.ts
+++ src/hooks/interface/use-edit-user-name-interface.ts
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 export function useEditUserNameInterface() {
 	const [edit_profile, edit_profile_setter] = useAtom(edit_profile_jotai);
 	const [user_name, user_name_setter] = useAtom(user_name_jotai);
-	const { id: userId, name } = useAtomValue(user_jotai);
+	const [{ id: userId, name }, user_setter] = useAtom(user_jotai);
 
 	function editName() {
 		edit_profile_setter("edit-name");
@@ -20,11 +20,14 @@ export function useEditUserNameInterface() {
 	async function saveNameEdit() {
 		try {
 			edit_profile_setter(null);
-			const { error } = await updateUserController(userId, {
+			const { error, user } = await updateUserController(userId, {
 				name: user_name,
 			});
 
-			if (error) toast.error("Update failed. Please try again later");
+			if (error) {
+				user_name_setter(name);
+				toast.error("Update failed. Please try again later");
+			} else user_setter(user);
 		} catch (error) {
 			user_name_setter(name);
 			toast.error("Update failed. Please try again later");
