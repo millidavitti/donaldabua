e2340d2c311e215e355c63ef8622cf8c98229e2b
davitti <abuadonald@gmail.com>
2025-02-23 22:25:40 +0100

    Add error handling to getUserLocation function

--- src/backend/get-user-location.controller.ts
+++ src/backend/get-user-location.controller.ts
@@ -1,10 +1,20 @@
 import { UserLocation } from "@/data/atoms/app_data";
 
 export async function getUserLocation(userId: string) {
-	const res = await fetch(
-		process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "/user-locations/" + userId,
-	);
-	const { location } = await res.json();
+	try {
+		const res = await fetch(
+			process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT +
+				"/user-locations/" +
+				userId,
+		);
+		const { location } = await res.json();
 
-	return location as UserLocation;
+		return location as UserLocation;
+	} catch (error) {
+		console.log("---getUserLocation---\n", getUserLocation);
+		return {
+			city: "Failed to fetch user location",
+			country: "Failed to fetch user",
+		} as UserLocation;
+	}
 }
