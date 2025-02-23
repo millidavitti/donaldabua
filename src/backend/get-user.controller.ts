d3429ee90b8556dc808a3215d15dbeef3a1a19d5
davitti <abuadonald@gmail.com>
2025-02-23 22:25:50 +0100

    Add error handling to getUser function

--- src/backend/get-user.controller.ts
+++ src/backend/get-user.controller.ts
@@ -1,12 +1,22 @@
 import { User } from "@/data/atoms/app_data";
 
 export async function getUser() {
-	const res = await fetch(
-		process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "/users/",
-	);
-	const {
-		users: [user],
-	} = await res.json();
+	try {
+		const res = await fetch(
+			process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "/users/",
+		);
+		const {
+			users: [user],
+		} = await res.json();
 
-	return user as User;
+		return user as User;
+	} catch (error) {
+		console.log("---getUser---\n", error);
+		return {
+			id: "",
+			image: "/stud.jpg",
+			name: "Failed to fetch user",
+			video: null,
+		} as User;
+	}
 }
