050517d54adeebb553bf1785fe5d95129027d367
davitti <abuadonald@gmail.com>
2025-02-24 03:23:40 +0100

    Refactor import path for DraftProject component.

--- src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
+++ src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/data/atoms/ui_state";
 import { useAtom, useSetAtom } from "jotai";
 import { CirclePlus } from "lucide-react";
-import DraftProject from "../add-project/draft-project-info";
+import DraftProject from "../add-project/draft-project";
 import PreviewProjectDraft from "../preview-project-draft/preview-project-draft";
 import ContentBuilder from "../content-builder/content-builder";
 import PublishedProjects from "../published-project/published-projects";
