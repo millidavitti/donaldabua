b22fed61f484140a67bf82f04aac595320109566
davitti <abuadonald@gmail.com>
2025-02-24 22:14:01 +0100

    Add custom hook for managing project technologies with fuzzy search functionality

---
+++ src/hooks/interface/use-add-project-technologies-interface.ts
@@ -0,0 +1,74 @@
+import {
+	project_technologies_jotai,
+	technologies_jotai,
+	Technology,
+	defaultStore,
+} from "@/data/atoms/app_data";
+import FuzzySearch from "fuzzy-search";
+import { useAtom, useAtomValue } from "jotai";
+import { useState } from "react";
+
+export default function useAddProjectTechnologiesInterface() {
+	const [project_technologies, project_technologies_setter] = useAtom(
+		project_technologies_jotai,
+	);
+	const technologies = useAtomValue(technologies_jotai);
+	const [hayStack, setHayStack] = useState<Technology[]>(technologies);
+	defaultStore.sub(technologies_jotai, () => {
+		setHayStack(defaultStore.get(technologies_jotai));
+	});
+	const [searchResult, setSearchResult] = useState<Technology[]>([]);
+	const [searchQuery, setSearchQuery] = useState<string>("");
+
+	function removeTechnology(tech: Technology) {
+		project_technologies_setter((technologies) =>
+			technologies.filter((technology) => technology.id !== tech.id),
+		);
+		setHayStack((technologies) => [...technologies, tech]);
+	}
+
+	function closeSearchResult(key: string) {
+		if (key === "Escape") setSearchResult([]);
+	}
+
+	function displaySearchResult() {
+		setTimeout(() => {
+			setSearchResult(hayStack);
+			document.onclick = (e) => {
+				if (!(e.target as HTMLElement).closest("#search-result"))
+					setSearchResult([]);
+				console.log("I was clicked!");
+				document.onclick = null;
+			};
+		}, 200);
+	}
+
+	function captureAndSearch(value: string) {
+		const search = new FuzzySearch(hayStack, ["name"]);
+		const result = search.search(value);
+		setSearchQuery(value);
+		if (value) setSearchResult(result);
+		else setSearchResult(hayStack);
+	}
+
+	function addTechnology(tech: Technology) {
+		project_technologies_setter((technology) => {
+			return [tech, ...technology];
+		});
+		setHayStack((technologies) =>
+			technologies.filter((technology) => technology.id !== tech.id),
+		);
+		setSearchQuery("");
+		setSearchResult([]);
+	}
+	return {
+		removeTechnology,
+		addTechnology,
+		captureAndSearch,
+		displaySearchResult,
+		closeSearchResult,
+		project_technologies,
+		searchResult,
+		searchQuery,
+	};
+}
