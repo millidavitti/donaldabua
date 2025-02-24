eec1ffb679e800415ed82d1bddfa2a0a1b9751b4
davitti <abuadonald@gmail.com>
2025-02-24 13:40:58 +0100

    Refactor EditProfileProjects to use new hooks and update header text.

--- src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
+++ src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
@@ -2,35 +2,26 @@
 import Flex from "@/components/layouts/flex";
 import InteractiveIcon from "@/components/layouts/interactive_icon";
 import Overlay from "@/components/layouts/overlay";
-import {
-	edit_profile_jotai,
-	project_form_step_jotai,
-} from "@/data/atoms/ui_state";
-import { useAtom, useSetAtom } from "jotai";
 import { CirclePlus } from "lucide-react";
 import DraftProject from "../add-project/draft-project";
 import PreviewProjectDraft from "../preview-project-draft/preview-project-draft";
 import ContentBuilder from "../content-builder/content-builder";
 import PublishedProjects from "../published-project/published-projects";
-import { useResetProjectFormFields } from "@/hooks/use-reset-project-form-fields";
+import { useEditProfileProjectsInterface } from "@/hooks/interface/use-edit-profile-projects-interface";
 
 export default function EditProfileProjects() {
-	const edit_profile_setter = useSetAtom(edit_profile_jotai);
-	const [project_form_step, project_form_step_setter] = useAtom(
-		project_form_step_jotai,
-	);
-	const resetProjectFormFields = useResetProjectFormFields();
+	const { addNewProject, project_form_step } =
+		useEditProfileProjectsInterface();
+
 	return (
 		<>
 			<Flex flex='column' className='gap-3'>
 				{/* Header */}
 				<Flex className='items-center justify-between shrink-0'>
-					<p className='font-semibold lg:text-2xl'>Portfolio</p>
+					<p className='font-semibold lg:text-2xl'>Projects</p>
 					<InteractiveIcon
 						callback={() => {
-							resetProjectFormFields();
-							edit_profile_setter("edit-portfolio");
-							project_form_step_setter("draft-project-info");
+							addNewProject();
 						}}
 					>
 						<CirclePlus size={24} />
