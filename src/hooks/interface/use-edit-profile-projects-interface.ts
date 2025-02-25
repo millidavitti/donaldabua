e36ce3bfa8ccccab01075d287df353e62456e946
davitti <abuadonald@gmail.com>
2025-02-25 02:16:46 +0100

    Refactor useEditProfileProjectsInterface to remove unused technology fetching logic.

--- src/hooks/interface/use-edit-profile-projects-interface.ts
+++ src/hooks/interface/use-edit-profile-projects-interface.ts
@@ -4,33 +4,18 @@ import {
 	edit_profile_jotai,
 	project_form_step_jotai,
 } from "@/data/atoms/ui_state";
-import { getTechnologiesController } from "@/backend/get-technologies.controller";
-import { toast } from "sonner";
-import { technologies_jotai } from "@/data/atoms/app_data";
 
 export function useEditProfileProjectsInterface() {
 	const edit_profile_setter = useSetAtom(edit_profile_jotai);
 	const [project_form_step, project_form_step_setter] = useAtom(
 		project_form_step_jotai,
 	);
-	const [technologies, technologies_setter] = useAtom(technologies_jotai);
 	const resetProjectFormFields = useResetProjectFormFields();
 
 	function addNewProject() {
 		resetProjectFormFields();
 		edit_profile_setter("edit-portfolio");
 		project_form_step_setter("draft-project-info");
-
-		if (!technologies.length)
-			getTechnologiesController().then((data) => {
-				const { error, technologies } = data;
-
-				if (error) {
-					toast.info(
-						"Unable to retrieve technologies at this time. Please try again later.",
-					);
-				} else technologies_setter(technologies);
-			});
 	}
 	return { addNewProject, project_form_step };
 }
