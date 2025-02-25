872b8ebd3117d69513d2e56932837237e83d0c97
davitti <abuadonald@gmail.com>
2025-02-25 15:57:30 +0100

    Add update project controller functionality.

---
+++ src/backend/update-project.controller.ts
@@ -0,0 +1,27 @@
+"use server";
+import { APIResponse, Project } from "@/data/atoms/app_data";
+
+export async function updateProjectController(
+	projectId: string,
+	update: Partial<Project>,
+) {
+	const headers = new Headers();
+	headers.append("Content-type", "application/json");
+
+	try {
+		const res = await fetch(
+			process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "/projects/" + projectId,
+			{
+				method: "PUT",
+				body: JSON.stringify(update),
+				headers,
+			},
+		);
+		const data = await res.json();
+
+		return data as APIResponse<Project, "update">;
+	} catch (error) {
+		console.log("---updateProjectController---\n", error);
+		throw error;
+	}
+}
