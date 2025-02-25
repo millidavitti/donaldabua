3f5f53b280d541de1dd083c51a005f409b196bbd
davitti <abuadonald@gmail.com>
2025-02-25 03:36:00 +0100

    Remove unused imports and clean up component code.

--- src/app/(dashboard)/components/published-project/published-project-tech-stack.tsx
+++ src/app/(dashboard)/components/published-project/published-project-tech-stack.tsx
@@ -1,8 +1,6 @@
 import Flex from "@/components/layouts/flex";
 import { project_technologies_jotai } from "@/data/atoms/app_data";
 import { useAtomValue } from "jotai";
-import { X } from "lucide-react";
-import React from "react";
 
 export default function PublishedProjectTechStack() {
 	const project_technologies = useAtomValue(project_technologies_jotai);
@@ -17,10 +15,6 @@ export default function PublishedProjectTechStack() {
 				{project_technologies.map((tech) => (
 					<Flex className='gap-3 items-center' key={tech.id}>
 						<p className='shrink-0 font-medium'>{tech.name}</p>
-						<X
-							size={24}
-							className='stroke-light-error cursor-pointer active:scale-[.95]'
-						/>
 					</Flex>
 				))}
 			</Flex>
