01236ec3c8d736d357c5ab00b38870ac32d4be3b
davitti <abuadonald@gmail.com>
2025-02-26 19:16:43 +0100

    Refactor savePublishedProjectEdit for improved project updates and error handling.

--- src/hooks/interface/use-publish-project-interface.tsx
+++ src/hooks/interface/use-publish-project-interface.tsx
@@ -53,17 +53,30 @@ export function usePublishProjectInterface() {
 
 	async function savePublishedProjectEdit() {
 		// Update Project
-		const { update, error } = await updateProjectController(
-			project_to_edit!.id,
-			{
-				title: defaultStore.get(project_title_jotai),
-				description: defaultStore.get(project_description_jotai),
-				thumbnail: defaultStore.get(project_thumbnail_jotai),
-			},
-		);
-		console.log("updatedProject---\n", update);
+		try {
+			const { update, error } = await updateProjectController(
+				project_to_edit!.id,
+				{
+					project: {
+						id: createId(),
+						title: defaultStore.get(project_title_jotai),
+						description: defaultStore.get(project_description_jotai),
+						thumbnail: defaultStore.get(project_thumbnail_jotai),
+					},
+					content: project_content,
+					technologies: project_technologies,
+				},
+			);
 
-		if (error) {
+			if (error) throw error;
+			else
+				projects_setter((projects) =>
+					projects.map((project) => {
+						if (project_to_edit!.id === project.id) return update;
+						return project;
+					}),
+				);
+		} catch (error) {
 			projects_setter((projects) =>
 				projects.map((project) => {
 					if (project_to_edit!.id === project.id) return project_to_edit!;
@@ -71,23 +84,8 @@ export function usePublishProjectInterface() {
 				}),
 			);
 			toast.error("Update failed. Please try again later");
-		} else
-			projects_setter((projects) =>
-				projects.map((project) => {
-					if (project_to_edit!.id === project.id) return update;
-					return project;
-				}),
-			);
+		}
 
-		// Update Project Technologies
-		const { info, error: updateError } =
-			await updateProjectTechnologiesController(
-				project_to_edit!.id,
-				project_technologies.map((technology) => technology.id),
-			);
-		console.log("updatedProjectTechnologies---\n", info);
-		console.log(updateError);
-		// Update Project Content
 		resetProjectFormFields();
 	}
 
