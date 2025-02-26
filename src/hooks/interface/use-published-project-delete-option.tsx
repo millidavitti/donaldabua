b447184f44c8a016f85f6b5f3180e483d0b7f3e3
davitti <abuadonald@gmail.com>
2025-02-26 13:10:05 +0100

    Enhance project deletion logic with error handling and notifications.

--- src/hooks/interface/use-published-project-delete-option.tsx
+++ src/hooks/interface/use-published-project-delete-option.tsx
@@ -1,12 +1,23 @@
+import { deleteProjectController } from "@/backend/delete-project.controller";
 import { projects_jotai } from "@/data/atoms/app_data";
 import { useSetAtom } from "jotai";
+import { toast } from "sonner";
 
 export default function usePublishedProjectDeleteOption() {
 	const projects_setter = useSetAtom(projects_jotai);
-	function deleteProject(projectID: string) {
-		projects_setter((projects) => {
-			return projects.filter((project) => project.id !== projectID);
-		});
+
+	async function deleteProject(projectID: string) {
+		try {
+			const { error } = await deleteProjectController(projectID);
+			if (error) throw error;
+			else
+				projects_setter((projects) => {
+					return projects.filter((project) => project.id !== projectID);
+				});
+		} catch (error) {
+			console.log("---deleteProject---\n", error);
+			toast.info("Unable to delete project. Please try again.");
+		}
 	}
 	return { deleteProject };
 }
