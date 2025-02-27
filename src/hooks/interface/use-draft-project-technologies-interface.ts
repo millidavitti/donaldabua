ecccdb9b4524a5dfffeb37653213f06fbc074585
davitti <abuadonald@gmail.com>
2025-02-27 01:16:52 +0100

    Refactor variable names for clarity in useDraftProjectTechnologiesInterface.

--- src/hooks/interface/use-draft-project-technologies-interface.ts
+++ src/hooks/interface/use-draft-project-technologies-interface.ts
@@ -1,7 +1,7 @@
 import {
 	project_technologies_jotai,
 	Technology,
-	hay_stack_jotai,
+	project_hay_stack_jotai,
 } from "@/data/atoms/app_data";
 import FuzzySearch from "fuzzy-search";
 import { useAtom } from "jotai";
@@ -11,7 +11,9 @@ export default function useDraftProjectTechnologiesInterface() {
 	const [project_technologies, project_technologies_setter] = useAtom(
 		project_technologies_jotai,
 	);
-	const [hay_stack, hay_stack_setter] = useAtom<Technology[]>(hay_stack_jotai);
+	const [project_hay_stack, project_hay_stack_setter] = useAtom<Technology[]>(
+		project_hay_stack_jotai,
+	);
 	const [searchResult, setSearchResult] = useState<Technology[]>([]);
 	const [searchQuery, setSearchQuery] = useState<string>("");
 
@@ -21,7 +23,6 @@ export default function useDraftProjectTechnologiesInterface() {
 				return technology.id !== tech.id;
 			}),
 		);
-		hay_stack_setter((technologies) => [...technologies, tech]);
 	}
 
 	function closeSearchResult(key: string) {
@@ -30,7 +31,7 @@ export default function useDraftProjectTechnologiesInterface() {
 
 	function displaySearchResult() {
 		setTimeout(() => {
-			setSearchResult(hay_stack);
+			setSearchResult(project_hay_stack);
 			document.onclick = (e) => {
 				if (!(e.target as HTMLElement).closest("#search-result"))
 					setSearchResult([]);
@@ -40,18 +41,18 @@ export default function useDraftProjectTechnologiesInterface() {
 	}
 
 	function captureAndSearch(value: string) {
-		const search = new FuzzySearch(hay_stack, ["name"]);
+		const search = new FuzzySearch(project_hay_stack, ["name"]);
 		const result = search.search(value);
 		setSearchQuery(value);
 		if (value) setSearchResult(result);
-		else setSearchResult(hay_stack);
+		else setSearchResult(project_hay_stack);
 	}
 
 	function addTechnology(tech: Technology) {
 		project_technologies_setter((technology) => {
 			return [tech, ...technology];
 		});
-		hay_stack_setter((technologies) =>
+		project_hay_stack_setter((technologies) =>
 			technologies.filter((technology) => technology.id !== tech.id),
 		);
 		setSearchQuery("");
