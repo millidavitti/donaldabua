fe1475fe7ab4f75ce2a40f6d7f6dfa828d68fe8e
davitti <abuadonald@gmail.com>
2025-02-27 09:07:00 +0100

    Refactor user video deletion to use user snapshot atom.

--- src/hooks/interface/use-delete-user-video-option-interface.ts
+++ src/hooks/interface/use-delete-user-video-option-interface.ts
@@ -1,25 +1,25 @@
 import { updateUserController } from "@/backend/update-user.controller";
-import { user_jotai, user_video_jotai } from "@/data/atoms/app_data";
+import { user_snapshot_jotai, user_video_jotai } from "@/data/atoms/app_data";
 import { useAtom, useSetAtom } from "jotai";
 import { toast } from "sonner";
 
 export function useDeleteUserVideoOptionInterface() {
 	const user_video_setter = useSetAtom(user_video_jotai);
-	const [{ id: userId, video }, user_setter] = useAtom(user_jotai);
+	const [user_snapshot, user_snapshot_setter] = useAtom(user_snapshot_jotai);
 
 	async function deleteVideo() {
 		user_video_setter(null);
 		try {
-			const { error, user } = await updateUserController(userId, {
+			const { error, user } = await updateUserController(user_snapshot.id, {
 				video: null,
 			});
 
 			if (error) {
-				user_video_setter(video);
+				user_video_setter(user_snapshot.video);
 				toast.error("Update failed. Please try again later");
-			} else user_setter(user);
+			} else user_snapshot_setter(user);
 		} catch (error) {
-			user_video_setter(video);
+			user_video_setter(user_snapshot.video);
 			toast.error("Update failed. Please try again later");
 			console.log("---saveVideoEdit---\n", error);
 		}
