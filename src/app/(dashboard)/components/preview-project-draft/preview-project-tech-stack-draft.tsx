d9bdc5c4f3a1fa441950fc28eca4f2df7c96aaea
davitti <abuadonald@gmail.com>
2025-02-27 22:59:39 +0100

    Refactor tech stack rendering to use ProfileTechnology component.

--- src/app/(dashboard)/components/preview-project-draft/preview-project-tech-stack-draft.tsx
+++ src/app/(dashboard)/components/preview-project-draft/preview-project-tech-stack-draft.tsx
@@ -3,9 +3,10 @@ import { project_technologies_jotai } from "@/data/atoms/app_data";
 import { useAtomValue } from "jotai";
 import { X } from "lucide-react";
 import React from "react";
+import ProfileTechnology from "../profile-technology";
 
 export default function PreviewProjectTechnologies() {
-	const project_tech_stack = useAtomValue(project_technologies_jotai);
+	const project_technologies = useAtomValue(project_technologies_jotai);
 
 	return (
 		<>
@@ -14,14 +15,8 @@ export default function PreviewProjectTechnologies() {
 				Tech Stack
 			</label>
 			<Flex className='gap-3 flex-wrap shrink-0'>
-				{project_tech_stack.map((tech) => (
-					<Flex className='gap-3 items-center' key={tech.id}>
-						<p className='shrink-0 font-medium'>{tech.name}</p>
-						<X
-							size={24}
-							className='stroke-light-error cursor-pointer active:scale-[.95]'
-						/>
-					</Flex>
+				{project_technologies.map((tech) => (
+					<ProfileTechnology tech={tech} key={tech.id} />
 				))}
 			</Flex>
 		</>
