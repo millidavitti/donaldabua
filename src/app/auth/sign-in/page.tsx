d97415ea09beb08efef4f66d029868143182806b
davitti <abuadonald@gmail.com>
2025-03-02 16:42:56 +0100

    Add Suspense wrapper to SignIn component and clean up form structure.

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/ui/button";
 import Flex from "@/components/layouts/flex";
 import { useSearchParams } from "next/navigation";
 import { toast } from "sonner";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 export default function SignIn() {
 	const params = useSearchParams();
@@ -12,20 +12,20 @@ export default function SignIn() {
 			toast.info("Your email has been verified", { position: "bottom-center" });
 	}, [params]);
 	return (
-		<form>
-			<Flex flex='column' className='gap-3'>
-				{/* <label htmlFor='name'>Full Name</label>
-				<input type='text' id='name' name='name' className='outline p-3' />{" "} */}
-				<label htmlFor='email'>Email</label>
-				<input
-					type='email'
-					id='email'
-					name='email'
-					className='outline p-3'
-					required
-				/>
-				<Button type='submit'>Sign In</Button>
-			</Flex>
-		</form>
+		<Suspense>
+			<form>
+				<Flex flex='column' className='gap-3'>
+					<label htmlFor='email'>Email</label>
+					<input
+						type='email'
+						id='email'
+						name='email'
+						className='outline p-3'
+						required
+					/>
+					<Button type='submit'>Sign In</Button>
+				</Flex>
+			</form>
+		</Suspense>
 	);
 }
