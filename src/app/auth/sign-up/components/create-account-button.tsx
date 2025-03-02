c4c77ee3a0e21350a4278810682dfa9c31f7af0b
davitti <abuadonald@gmail.com>
2025-03-02 16:31:37 +0100

    Add CreateAccountButton component for user sign-up functionality

---
+++ src/app/auth/sign-up/components/create-account-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { signUpController } from "@/backend/auth/sign-up.controller";
+import Button from "@/components/ui/button";
+import React from "react";
+import { toast } from "sonner";
+
+export default function CreateAccountButton() {
+	return (
+		<Button
+			type='submit'
+			htmlProps={{
+				async formAction(formData) {
+					try {
+						await signUpController(formData);
+						toast.info("A verification email has been sent");
+					} catch (error) {
+						console.log("---CreateAccountButton---\n", (error as any).message);
+						if ((error as any).message.includes("duplicate"))
+							toast.info("Email already exists. Try signing in ");
+						else toast.info("Unable to create your account at the moment ");
+					}
+				},
+			}}
+		>
+			Create Account
+		</Button>
+	);
+}
