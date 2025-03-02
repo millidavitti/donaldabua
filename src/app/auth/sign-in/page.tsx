9983ea3770b78c2e7bb52b9f0cfb2d19db5fb973
davitti <abuadonald@gmail.com>
2025-03-02 16:47:39 +0100

    Refactor SignIn component to use SignInForm and remove unused imports.

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,31 +1,10 @@
-"use client";
-import Button from "@/components/ui/button";
-import Flex from "@/components/layouts/flex";
-import { useSearchParams } from "next/navigation";
-import { toast } from "sonner";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
+import SignInForm from "./components/sign-in-form";
 
 export default function SignIn() {
-	const params = useSearchParams();
-	useEffect(() => {
-		if (params.get("verified"))
-			toast.info("Your email has been verified", { position: "bottom-center" });
-	}, [params]);
 	return (
 		<Suspense>
-			<form>
-				<Flex flex='column' className='gap-3'>
-					<label htmlFor='email'>Email</label>
-					<input
-						type='email'
-						id='email'
-						name='email'
-						className='outline p-3'
-						required
-					/>
-					<Button type='submit'>Sign In</Button>
-				</Flex>
-			</form>
+			<SignInForm />
 		</Suspense>
 	);
 }
