c15dfc9f391b2a11a94d1944717dc1fe47bea4d4
davitti <abuadonald@gmail.com>
2025-03-06 23:31:55 +0100

    Refactor email verification route to use GET method and improved error handling.

--- src/app/auth/verify-email/route.ts
+++ src/app/auth/verify-email/route.ts
@@ -1,14 +1,40 @@
+import { getErrorMessage } from "@/utils/get-error-message";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-	const searchParams = req.nextUrl.searchParams;
+	console.log("---ORIGIN---\n", Object.fromEntries(req.headers.entries()));
+	try {
+		const searchParams = req.nextUrl.searchParams;
+		const res = await fetch(
+			`${process.env.AUTH_ENDPOINT!}/verify-email/${searchParams.get(
+				"email",
+			)}/${searchParams.get("verification-token")}`,
+			{ method: "GET", headers: req.headers },
+		);
 
-	const res = await fetch(
-		process.env.NEXT_PUBLIC_AUTH_ENDPOINT! +
-			"/verify-email/" +
-			searchParams.get("email"),
-		{ method: "POST" },
-	);
-	await res.json();
-	return Response.redirect("http://localhost:3000/auth/sign-in?verified=true");
+		const { error, status } = await res.json();
+
+		if (error) throw new Error(error);
+
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: `${process.env.ORIGIN}/auth/sign-in?verified=${status}`,
+				"Set-cookie":
+					"connect.sid=; path=/; expires=Thu, 1 Jan 1970 00:00:00 GMT;",
+			},
+		});
+	} catch (error) {
+		console.error("---verifyEmailRoute---\n", error);
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: `${
+					process.env.ORIGIN
+				}/auth/sign-in?verificationError=${getErrorMessage(error)}`,
+				"Set-Cookie":
+					"connect.sid=; path=/; expires=Thu, 1 Jan 1970 00:00:00 GMT;",
+			},
+		});
+	}
 }
