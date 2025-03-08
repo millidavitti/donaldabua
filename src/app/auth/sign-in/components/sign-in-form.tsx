2dc59a70fec8fe539c6a2b5bfc2df5525919af4b
davitti <abuadonald@gmail.com>
2025-03-08 20:45:03 +0100

    Refactor sign-in form to handle email input and loading state.

--- src/app/auth/sign-in/components/sign-in-form.tsx
+++ src/app/auth/sign-in/components/sign-in-form.tsx
@@ -4,24 +4,29 @@ import Flex from "@/components/layouts/flex";
 import Button from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
 import { toast } from "sonner";
 
 export default function SignInForm() {
 	const params = useSearchParams();
 	const [signIn, setSignIn] = useState(false);
+	const [formData, setFormData] = useState({ email: "" });
+
 	useEffect(() => {
-		if (params.get("verified") === "true")
+		if (params.get("status") === "Email verified")
 			toast.info("Your email has been verified", { position: "bottom-center" });
 	}, [params]);
 	return (
 		<form
-			action={async (formData) => {
+			onSubmit={async (e) => {
+				e.preventDefault();
+				setSignIn(true);
 				try {
-					setSignIn(true);
-					await signInController(formData);
-					toast.info("A magic link has been sent");
+					const { status } = await signInController(formData);
+					toast.info(status);
+					setSignIn(false);
 				} catch (error) {
-					console.log("---CreateAccountButton---\n", error);
+					console.error("---SignInForm---\n", error);
 					toast.info("Unable to sign you in to account at the moment");
 				}
 			}}
@@ -34,9 +39,10 @@ export default function SignInForm() {
 					name='email'
 					className='outline p-3'
 					required
+					onChange={(e) => setFormData({ email: e.target.value })}
 				/>
-				<Button type='submit' htmlProps={{ disabled: signIn }}>
-					Sign In
+				<Button type='submit'>
+					Sign In {signIn && <HashLoader size={24} />}
 				</Button>
 			</Flex>
 		</form>
