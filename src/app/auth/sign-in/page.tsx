4b9281d1619488d964d5dacea3db211e9e5541f3
davitti <abuadonald@gmail.com>
2025-03-08 20:43:58 +0100

    Add Sign Up button to Sign In page layout

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from "react";
 import SignInForm from "./components/sign-in-form";
+import Flex from "@/components/layouts/flex";
+import Button from "@/components/ui/button";
+import Link from "next/link";
 
 export default function SignIn() {
 	return (
 		<Suspense>
-			<SignInForm />
+			<Flex flex='column' className='gap-3'>
+				<SignInForm />
+				<Link href='/auth/sign-up'>
+					<Button type='button'>Sign Up</Button>
+				</Link>
+			</Flex>
 		</Suspense>
 	);
 }
