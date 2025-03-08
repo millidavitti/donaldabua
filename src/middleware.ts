0cc1d710420f4624995d2e17f60e7dd5d29e6772
davitti <abuadonald@gmail.com>
2025-03-08 16:32:16 +0100

    Add authentication middleware for user redirect logic.

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./utils/auth";
+
+export async function middleware(request: NextRequest) {
+	const { isAuthenticated, user } = await auth(request);
+	console.log("---isAuthenticated---\n", isAuthenticated, user);
+	if (isAuthenticated) {
+		if (
+			request.nextUrl.pathname === "/auth/sign-in" ||
+			request.nextUrl.pathname === "/auth/sign-in" ||
+			request.nextUrl.pathname === "/"
+		)
+			return NextResponse.redirect(new URL("/dashboard", request.url));
+	} else {
+		if (
+			request.nextUrl.pathname === "/dashboard" ||
+			request.nextUrl.pathname === "/"
+		) {
+			return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+		}
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/", "/auth/sign-in", "/auth/sign-up", "/dashboard"],
+};
