3af2bc0b08e7f4b9b41b39ff99f84fb202702196
davitti <abuadonald@gmail.com>
2025-03-08 01:11:22 +0100

    Add sign-in functionality with email verification handling.

--- src/app/auth/sign-in/components/sign-in-form.tsx
+++ src/app/auth/sign-in/components/sign-in-form.tsx
@@ -1,18 +1,31 @@
 "use client";
+import { signInController } from "@/backend/auth/sign-in.controller";
 import Flex from "@/components/layouts/flex";
 import Button from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function SignInForm() {
 	const params = useSearchParams();
+	const [signIn, setSignIn] = useState(false);
 	useEffect(() => {
-		if (params.get("verified"))
+		if (params.get("verified") === "true")
 			toast.info("Your email has been verified", { position: "bottom-center" });
 	}, [params]);
 	return (
-		<form>
+		<form
+			action={async (formData) => {
+				try {
+					// setSignIn(true);
+					await signInController(formData);
+					toast.info("A magic link has been sent");
+				} catch (error) {
+					console.log("---CreateAccountButton---\n", error);
+					toast.info("Unable to sign you in to account at the moment");
+				}
+			}}
+		>
 			<Flex flex='column' className='gap-3'>
 				<label htmlFor='email'>Email</label>
 				<input
@@ -22,7 +35,9 @@ export default function SignInForm() {
 					className='outline p-3'
 					required
 				/>
-				<Button type='submit'>Sign In</Button>
+				<Button type='submit' htmlProps={{ disabled: signIn }}>
+					Sign In
+				</Button>
 			</Flex>
 		</form>
 	);
