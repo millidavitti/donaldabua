945a1b56cf7a927c8d24876cdca49e692e1155a7
davitti <abuadonald@gmail.com>
2025-03-11 16:39:00 +0100

    Add signOut function to auth utility and update error logging.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -16,8 +16,27 @@ export async function auth(request?: NextRequest) {
 
 		return data as auth;
 	} catch (error) {
-		console.error("---isAuth---", error);
+		console.error("---auth---", error);
 		return { error: getErrorMessage(error) } as auth;
+	}
+}
+
+export async function signOut() {
+	try {
+		const res = await fetch(
+			process.env.NEXT_PUBLIC_AUTH_ENDPOINT! + "/sign-out",
+			{
+				method: "GET",
+				credentials: "include",
+			},
+		);
+		const { error, isAuthenticated } = await res.json();
+		console.log({ error, isAuthenticated });
+		if (error) throw new Error(error);
+		else if (isAuthenticated) return { isAuthenticated };
+	} catch (error) {
+		console.error("---signOut---", error);
+		throw new Error(getErrorMessage(error));
 	}
 }
 
