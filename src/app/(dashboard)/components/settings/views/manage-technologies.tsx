5862925abe980fa7a41766342db1f7e5db8ce4ea
davitti <abuadonald@gmail.com>
2025-03-11 04:00:53 +0100

    Add ManageTechnologies component with form and save functionality

---
+++ src/app/(dashboard)/components/settings/views/manage-technologies.tsx
@@ -0,0 +1,43 @@
+import Flex from "@/components/layouts/flex";
+import InteractiveIcon from "@/components/layouts/interactive_icon";
+import Overlay from "@/components/layouts/overlay";
+import { settings_view_jotai } from "@/data/atoms/ui_state";
+import { useSetAtom } from "jotai";
+import React from "react";
+import AddTechnologies from "../add-technologies";
+import Button from "@/components/ui/button";
+import { X } from "lucide-react";
+import useManageTechnologiesInterface from "@/hooks/interface/use-manage-technologies-interface";
+import { HashLoader } from "react-spinners";
+
+export default function ManageTechnologies() {
+	const { close, createTechnologies, api_task } =
+		useManageTechnologiesInterface();
+	return (
+		<Overlay stateFlag='manage-technologies'>
+			<Flex
+				flex='column'
+				className='bg-light-surface gap-3 basis-[720px] neonScan'
+			>
+				<Flex className='justify-between items-center shrink-0'>
+					<h2 className='text-2xl font-semibold'>Technologies</h2>
+					<InteractiveIcon callback={() => close()}>
+						<X size={24} className='stroke-light-error' />
+					</InteractiveIcon>
+				</Flex>
+				<form
+					className='flex flex-col gap-3'
+					onSubmit={(e) => {
+						e.preventDefault();
+						createTechnologies();
+					}}
+				>
+					<AddTechnologies />
+					<Button type='submit' className='bg-black text-light-surface'>
+						Save {api_task && <HashLoader size={24} color='ffff' />}
+					</Button>
+				</form>
+			</Flex>
+		</Overlay>
+	);
+}
