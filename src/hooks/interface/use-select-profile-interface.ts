2bb12ba299cf1d11a9bd87fa959794648af4226f
davitti <abuadonald@gmail.com>
2025-03-12 00:30:11 +0100

    Add custom hook for selecting user profile interface.

---
+++ src/hooks/interface/use-select-profile-interface.ts
@@ -0,0 +1,26 @@
+import {
+	profile_snapshot_jotai,
+	profiles_jotai,
+	UserProfile,
+} from "@/data/atoms/app_data";
+import { dashboard_view_jotai } from "@/data/atoms/ui_state";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+
+export default function useSelectProfileInterface() {
+	const dashboard_view_setter = useSetAtom(dashboard_view_jotai);
+	const profile_snapshot_setter = useSetAtom(profile_snapshot_jotai);
+	const profiles = useAtomValue(profiles_jotai);
+	function display() {
+		dashboard_view_setter("select-profile");
+	}
+
+	function close() {
+		dashboard_view_setter(null);
+	}
+
+	function select(profile: UserProfile) {
+		profile_snapshot_setter(profile);
+		close();
+	}
+	return { display, close, profiles, select };
+}
