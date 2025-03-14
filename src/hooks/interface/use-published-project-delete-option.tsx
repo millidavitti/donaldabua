111f9bd1a882a7ecf8d0dd6d6b6ed47c21f584f8
davitti <abuadonald@gmail.com>
2025-03-14 23:04:41 +0100

    feat(hook): enhance published project delete option
    - implement dialog display for delete confirmation
    - add task waiting mechanism before project deletion
    - return api_task state for better UI feedback

--- src/hooks/interface/use-published-project-delete-option.tsx
+++ src/hooks/interface/use-published-project-delete-option.tsx
@@ -1,23 +1,30 @@
 import { deleteProjectController } from "@/backend/delete-project.controller";
 import { projects_snapshot_jotai } from "@/data/atoms/app_data";
-import { useSetAtom } from "jotai";
+import { api_task_jotai } from "@/data/atoms/ui_state";
+import { waitForTask } from "@/utils/wait-for-task";
+import { useAtomValue, useSetAtom } from "jotai";
 import { toast } from "sonner";
+import useDialog from "../use-dialog";
 
 export default function usePublishedProjectDeleteOption() {
 	const projects_snapshot_setter = useSetAtom(projects_snapshot_jotai);
-
+	const api_task = useAtomValue(api_task_jotai);
+	const { closeDialog, displayDialog } = useDialog();
 	async function deleteProject(projectID: string) {
-		try {
-			const { error } = await deleteProjectController(projectID);
-			if (error) throw error;
+		displayDialog("delete-project");
+		if (await new Promise(waitForTask()))
+			try {
+				const { error } = await deleteProjectController(projectID);
+				if (error) throw error;
 
-			projects_snapshot_setter((projects) => {
-				return projects.filter((project) => project.id !== projectID);
-			});
-		} catch (error) {
-			console.error("---deleteProject---\n", error);
-			toast.info("Unable to delete project. Please try again.");
-		}
+				projects_snapshot_setter((projects) => {
+					return projects.filter((project) => project.id !== projectID);
+				});
+			} catch (error) {
+				console.error("---deleteProject---\n", error);
+				toast.info("Unable to delete project. Please try again.");
+			}
+		closeDialog();
 	}
-	return { deleteProject };
+	return { deleteProject, api_task };
 }
