bba30288983baa37522b72484eb2deebb2382c74
davitti <abuadonald@gmail.com>
2025-03-14 19:13:51 +0100

    refactor(animations): clean up animation class definition
    - remove redundant type definition for AnimationClass
    - import AnimationClass from constants for better clarity

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -1,6 +1,5 @@
+import { AnimationClass } from "@/data/constants";
+
 export function getAnimationClass(className: AnimationClass) {
 	return className;
 }
-
-const ANIMATIONS = ["neonScan", "swing-in-top-fwd"] as const;
-type AnimationClass = (typeof ANIMATIONS)[number];
