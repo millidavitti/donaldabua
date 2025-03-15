6199ff248582c608f7fd73c84a3f37cd8b7e845d
davitti <abuadonald@gmail.com>
2025-03-15 02:15:32 +0100

    ♻️ refactor(hooks): simplify useDialog hook implementation
    - remove unused api_task_jotai import and setter
    - update displayDialog function to remove apiTask parameter

--- src/hooks/use-dialog.ts
+++ src/hooks/use-dialog.ts
@@ -1,23 +1,16 @@
-import {
-	api_task_jotai,
-	ApiTask,
-	dashboard_view_jotai,
-	dialog_jotai,
-} from "@/data/atoms/ui_state";
+import { dashboard_view_jotai, dialog_jotai } from "@/data/atoms/ui_state";
 import { useSetAtom } from "jotai";
 
 export default function useDialog() {
-	const api_task_setter = useSetAtom(api_task_jotai);
 	const dashboard_view_setter = useSetAtom(dashboard_view_jotai);
 	const dialog_setter = useSetAtom(dialog_jotai);
 
-	function displayDialog(apiTask: ApiTask) {
+	function displayDialog() {
 		dashboard_view_setter("alert-dialog");
-		api_task_setter(apiTask);
 	}
+
 	function closeDialog() {
 		dashboard_view_setter(null);
-		api_task_setter(null);
 		dialog_setter(null);
 	}
 	return { closeDialog, displayDialog };
