5a89e30d6cdc76a377117e4e929796f0a455fc46
davitti <abuadonald@gmail.com>
2025-03-15 02:15:53 +0100

    ♻️ refactor(hooks): simplify alert dialog interface
    - remove unused api_task and its setter from the interface
    - streamline proceed function to eliminate unnecessary parameter

--- src/hooks/interface/use-alert-dialog-interface.ts
+++ src/hooks/interface/use-alert-dialog-interface.ts
@@ -1,26 +1,18 @@
-import {
-	api_task_jotai,
-	ApiTask,
-	dashboard_view_jotai,
-	dialog_jotai,
-} from "@/data/atoms/ui_state";
+import { dashboard_view_jotai, dialog_jotai } from "@/data/atoms/ui_state";
 import { useAtom, useSetAtom } from "jotai";
 
 export default function useAlertDialogInterface() {
 	const dashboard_view_setter = useSetAtom(dashboard_view_jotai);
-	const [api_task, api_task_setter] = useAtom(api_task_jotai);
 	const [dialog, dialog_setter] = useAtom(dialog_jotai);
 
 	function cancel() {
 		dashboard_view_setter(null);
-		api_task_setter(null);
 		dialog_setter("cancel");
 	}
 
-	function proceed(apiTask: ApiTask) {
-		api_task_setter(apiTask);
+	function proceed() {
 		dialog_setter("continue");
 	}
 
-	return { cancel, proceed, dialog, api_task };
+	return { cancel, proceed, dialog };
 }
