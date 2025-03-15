819965526b618b16d19c55ada6934f326e4f4480
davitti <abuadonald@gmail.com>
2025-03-16 00:01:30 +0100

    feat: add content builder components
    - Implemented `ContentImage` for managing and displaying project images
    - Added `ContentMarkdown` to support markdown-based content with live editing
    - Introduced `ContentVideo` for embedding and validating YouTube videos
    - Created `Content` component to handle dynamic rendering of content blocks
    - Integrated Jotai for state management of project content

---
+++ src/app/(home)/components/content/content-image.tsx
@@ -0,0 +1,70 @@
+import Flex from "@/components/layouts/flex";
+import Image from "next/image";
+import { useState } from "react";
+import { project_content_jotai, ProjectImage } from "@/data/atoms/app_data";
+import { useAtom, useSetAtom } from "jotai";
+import { component_to_edit_jotai } from "@/data/atoms/ui_state";
+import Button from "@/components/ui/button";
+
+interface ContentImage {
+	component: ProjectImage;
+}
+export default function ContentImage({ component }: ContentImage) {
+	const [component_to_edit, component_to_edit_setter] = useAtom(
+		component_to_edit_jotai,
+	);
+	const [imageLink, setImageLink] = useState(component.url);
+	const project_content_setter = useSetAtom(project_content_jotai);
+	return (
+		<Flex flex='column' className='relative'>
+			{component_to_edit === component.id || (
+				<Image
+					src={component.url}
+					width={1000}
+					height={1000}
+					alt=''
+					className='neonScan'
+				/>
+			)}
+			{component_to_edit === component.id && (
+				<Flex flex='column' className='bg-light-surface gap-3 neonScan'>
+					<Flex flex='column' className='gap-3'>
+						<label className='text-xl font-semibold' htmlFor='title'>
+							Paste a link to your image
+						</label>
+						<input
+							type='url'
+							id='title'
+							required
+							value={imageLink}
+							onChange={(e) => {
+								setImageLink(e.target.value);
+							}}
+							className='outline p-3'
+						/>
+
+						<Button
+							className='bg-black text-light-surface'
+							onClick={() => {
+								project_content_setter((content) => {
+									return content.map((obj) => {
+										if (component.id === obj.id)
+											return {
+												...obj,
+												url: imageLink,
+											};
+										return obj;
+									});
+								});
+								setImageLink("");
+								component_to_edit_setter(null);
+							}}
+						>
+							Save
+						</Button>
+					</Flex>
+				</Flex>
+			)}
+		</Flex>
+	);
+}
---
+++ src/app/(home)/components/content/content-markdown.tsx
@@ -0,0 +1,53 @@
+import Flex from "@/components/layouts/flex";
+import React, { useState } from "react";
+import { component_to_edit_jotai } from "@/data/atoms/ui_state";
+import { project_content_jotai, ProjectMarkdown } from "@/data/atoms/app_data";
+import { useAtom, useSetAtom } from "jotai";
+import md from "md";
+import parse from "html-react-parser";
+
+interface ContentMarkdown {
+	component: ProjectMarkdown;
+}
+export default function ContentMarkdown({ component }: ContentMarkdown) {
+	const [component_to_edit, component_to_edit_setter] = useAtom(
+		component_to_edit_jotai,
+	);
+	const project_content_setter = useSetAtom(project_content_jotai);
+	const [markdown, setMarkdown] = useState(component.markdown);
+	return (
+		<Flex
+			flex='column'
+			className='gap-3 relative'
+			htmlProps={{
+				onKeyDown(e) {
+					if (e.key === "Enter" && e.ctrlKey) {
+						component_to_edit_setter(null);
+						project_content_setter((content) => {
+							return content.map((obj) => {
+								if (component.id === obj.id)
+									return {
+										...obj,
+										markdown,
+									};
+								return obj;
+							});
+						});
+					}
+				},
+			}}
+		>
+			{component_to_edit === component.id || parse(md(component.markdown))}
+			{component_to_edit === component.id && (
+				<textarea
+					value={markdown}
+					className='shrink-0 p-3 neonScan'
+					rows={30}
+					onChange={(e) => {
+						setMarkdown(e.target.value);
+					}}
+				/>
+			)}
+		</Flex>
+	);
+}
