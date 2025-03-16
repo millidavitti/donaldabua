38729c9116e91e03ae561b5368290a59dc973848
davitti <abuadonald@gmail.com>
2025-03-16 02:25:23 +0100

    feat: add Project component
    - Introduced `Project` component to display project details
    - Implements animations using `getAnimationClass` for smooth entry effects
    - Uses `usePublishedProjectInterface` to handle project view interactions
    - Displays project title and thumbnail with responsive styling

---
+++ src/app/(home)/components/project/project.tsx
@@ -0,0 +1,49 @@
+import Flex from "@/components/layouts/flex";
+import Image from "next/image";
+import { type Project } from "@/data/atoms/app_data";
+import usePublishedProjectInterface from "@/hooks/interface/use-published-project-interface";
+import { DELAY } from "@/data/constants";
+import { cn } from "@/utils/cn";
+import { getAnimationClass } from "@/utils/animations";
+
+interface IProject {
+	project: Project;
+	index: number;
+}
+export default function Project({ project, index }: IProject) {
+	const { viewProject } = usePublishedProjectInterface();
+
+	return (
+		<Flex
+			className={cn(
+				"shrink-0 gap-3 grow w-full md:basis-52 relative",
+				getAnimationClass("swing-in-top-fwd"),
+			)}
+			htmlProps={{
+				style: { animationDelay: index * DELAY + "ms" },
+			}}
+		>
+			{/* Project */}
+			<Flex
+				flex='column'
+				className='gap-3 grow md:basis-52 cursor-pointer active:scale-[.98] transition'
+				htmlProps={{
+					onClick() {
+						viewProject(project);
+					},
+				}}
+			>
+				<Flex className='w-full h-40 shrink-0'>
+					<Image
+						src={project.thumbnail}
+						width={1000}
+						height={1000}
+						alt='portfolio-project-thumbnail'
+						className='object-cover'
+					/>
+				</Flex>
+				<p className='text-lg font-semibold'>{project.title}</p>
+			</Flex>
+		</Flex>
+	);
+}
