4bf9f958de5154278fef367d936f7b666b345b6e
davitti <abuadonald@gmail.com>
2025-03-16 23:05:37 +0100

    refactor(endpoints): remove `user-location.endpoints.ts`
    - Moved user location endpoints to `user/location.endpoints.ts`
    - Updated references to use `LOCATION_ENDPOINTS` instead
    - Improves code organization by grouping related user endpoints under `user/`

--- src/backend/endpoints/user-location.endpoints.ts
+++
@@ -1,6 +0,0 @@
-import { BASE_URL } from ".";
-
-export const USER_LOCATION_ENDPOINTS = {
-	get: (userId: string) => `${BASE_URL}/user-locations/public/${userId}`,
-	update: (userId: string) => `${BASE_URL}/user-locations/private/${userId}`,
-};
