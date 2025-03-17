10c573e4da0c99b1fc15f844ab2b171a2487e1fa
davitti <abuadonald@gmail.com>
2025-03-17 07:42:03 +0100

    ✨ feat(home): implement project vault layout
    - replace users list with project vault components
    - add VaultHeader, User, Sidebar, ProfileSummary, ProfileProjects, and ProfileTechnologies
    - integrate SelectUser component for user selection functionality
    - use jotai for state management with user snapshot

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,42 @@
-import { getUsersController } from "@/backend/controllers/user/get-users.controller";
+"use client";
 import Flex from "@/components/layouts/flex";
-import Link from "next/link";
-import React from "react";
+import VaultHeader from "./components/vault-header";
+import User from "./components/user/user";
+import Sidebar from "./components/sidebar";
+import ProfileSummary from "./components/profile/profile-summary";
+import ProfileProjects from "./components/profile/profile-projects";
+import ProfileTechnologies from "./components/profile/profile-technologies";
+import SelectUser from "./components/select-user";
+import { useAtomValue } from "jotai";
+import { cn } from "@/utils/cn";
+import { getAnimationClass } from "@/utils/animations";
+import { user_snapshot_jotai } from "@/data/home/home-atoms/home-data";
 
-export default async function HomePage() {
-	const users = await getUsersController();
+export default function ProjectVault() {
+	const user_snapshot = useAtomValue(user_snapshot_jotai);
 	return (
-		<Flex>
-			{users.users.map((user) => (
-				<Link href={`/${user.id}`} key={user.id}>
-					{user.name}
-				</Link>
-			))}
-		</Flex>
+		<>
+			{Boolean(user_snapshot.id) && (
+				<Flex
+					className={cn("w-full h-full gap-3", getAnimationClass("neonScan"))}
+					flex='column'
+				>
+					<VaultHeader />
+					<Flex className='outline h-full gap-3' flex='column'>
+						<User />
+						{/* Main */}
+						<Flex className='grow gap-3 shrink-0 flex-wrap h-fit'>
+							<Sidebar />
+							<Flex className='basis-[720px] grow-[2] gap-3' flex='column'>
+								<ProfileSummary />
+								<ProfileProjects />
+								<ProfileTechnologies />
+							</Flex>
+						</Flex>
+					</Flex>
+				</Flex>
+			)}
+			<SelectUser />
+		</>
 	);
 }
