b29f9d1e889000d5238d97ce1b10305aa6f25db0
davitti <abuadonald@gmail.com>
2025-03-17 02:08:35 +0100

    ♻️ refactor(user): update socials endpoint paths
    - change user-socials paths to socials for consistency
    - improve clarity in endpoint structure for better usability privateProjectTechnologyRoutes

--- src/backend/endpoints/user/socials.endpoint.ts
+++ src/backend/endpoints/user/socials.endpoint.ts
@@ -1,10 +1,10 @@
 import { BASE_URL } from "..";
 
 export const SOCIALS_ENDPOINTS = {
-	list: (userId: string) => `${BASE_URL}/user-socials/public/${userId}`,
-	create: (userId: string) => `${BASE_URL}/user-socials/private/${userId}`,
+	list: (userId: string) => `${BASE_URL}/socials/public/${userId}`,
+	create: (userId: string) => `${BASE_URL}/socials/private/${userId}`,
 	update: (socialAccountId: string) =>
-		`${BASE_URL}/user-socials/private/${socialAccountId}`,
+		`${BASE_URL}/socials/private/${socialAccountId}`,
 	delete: (socialAccountId: string) =>
-		`${BASE_URL}/user-socials/private/${socialAccountId}`,
+		`${BASE_URL}/socials/private/${socialAccountId}`,
 };
