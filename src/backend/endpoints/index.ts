85c2265b9d007e4e9dc2ccf950cd47876294114f
davitti <abuadonald@gmail.com>
2025-03-17 02:29:08 +0100

    refactor(endpoints): remove unused BASE_URL from index.ts
    - Deleted BASE_URL as it was unused in the endpoints configuration
    - Cleaned up redundant code for better maintainability

--- src/backend/endpoints/index.ts
+++ src/backend/endpoints/index.ts
@@ -2,10 +2,7 @@ import { LOCATION_ENDPOINTS } from "./user/location.endpoints";
 import { SOCIALS_ENDPOINTS } from "./user/socials.endpoint";
 import { USER_ENDPOINTS } from "./user/user.endpoints";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT + "";
-
 export const ENDPOINTS = {
-	base: BASE_URL,
 	user: USER_ENDPOINTS,
 	location: LOCATION_ENDPOINTS,
 	socials: SOCIALS_ENDPOINTS,
