d5925702dd2ffd7cd0b748d9a6fdc96e1d7d33e3
davitti <abuadonald@gmail.com>
2025-03-18 00:51:58 +0100

    feat(dashboard): add usePublishedProjectInterface hook
    - Created dashboard-specific hook for viewing published projects
    - Fetches project content and technologies asynchronously
    - Manages project state using Jotai atoms
    - Displays toast no

---
+++ src/hooks/interface/dashboard/use-published-project-interface.ts
@@ -0,0 +1,43 @@
+import { getProjectContentController } from "@/backend/controllers/dashboard/project/get-project-content.controller";
+import { getProjectTechnologiesController } from "@/backend/controllers/dashboard/project/get-project-technologies.controller";
+import {
+	project_content_jotai,
+	project_technologies_jotai,
+	Project,
+	project_snapshot_jotai,
+} from "@/data/dashboard/dashboard-atoms/dashboard-data";
+import { dashboard_view_jotai } from "@/data//dashboard/dashboard-atoms/dashboard-ui-state";
+import { useSetAtom } from "jotai";
+import { toast } from "sonner";
+
+export default function usePublishedProjectInterface() {
+	const dashboard_view_setter = useSetAtom(dashboard_view_jotai);
+	const project_snapshot_setter = useSetAtom(project_snapshot_jotai);
+	const project_content_setter = useSetAtom(project_content_jotai);
+	const project_technologies_setter = useSetAtom(project_technologies_jotai);
+
+	async function viewProject(project: Project) {
+		dashboard_view_setter("view-project");
+		try {
+			const [
+				{ projectTechnologies, error: projectTechnologiesError },
+				{ projectContent, error: projectContentError },
+			] = await Promise.all([
+				getProjectTechnologiesController(project.id),
+				getProjectContentController(project.id),
+			]);
+
+			if (projectTechnologiesError || projectContentError)
+				throw projectTechnologiesError || projectContentError;
+
+			project_technologies_setter(projectTechnologies);
+			project_content_setter(projectContent);
+		} catch (error) {
+			dashboard_view_setter(null);
+			console.error("---editProject:getProjectContent---\n", error);
+			toast.info("Unable to retrieve project content. Please try again later.");
+		}
+		project_snapshot_setter(project);
+	}
+	return { viewProject };
+}
