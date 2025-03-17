ff9f4fea1c98a59c5749941ef4923e6bed3ab192
davitti <abuadonald@gmail.com>
2025-03-17 07:43:03 +0100

    ♻️ refactor(user): rename user image atom for clarity
    - change user_image_jotai to user_snapshot_jotai for better context
    - update variable usage to reflect new atom naming

--- src/app/(home)/components/user/user-photo.tsx
+++ src/app/(home)/components/user/user-photo.tsx
@@ -1,10 +1,10 @@
 import Flex from "@/components/layouts/flex";
-import { user_image_jotai } from "@/data/home/home-atoms/home-data";
+import { user_snapshot_jotai } from "@/data/home/home-atoms/home-data";
 import { useAtomValue } from "jotai";
 import Image from "next/image";
 
 export default function UserPhoto() {
-	const user_image = useAtomValue(user_image_jotai);
+	const user_snapshot = useAtomValue(user_snapshot_jotai);
 
 	return (
 		<Flex
@@ -12,9 +12,9 @@ export default function UserPhoto() {
 			className='relative overflow-visible z-10 mx-auto lg:mx-0'
 		>
 			<Flex className='rounded-full shrink-0 p-0 h-24 w-24 cursor-pointer active:scale-[.99] overflow-clip'>
-				{Boolean(user_image) && (
+				{Boolean(user_snapshot.image) && (
 					<Image
-						src={user_image}
+						src={user_snapshot.image}
 						width={1000}
 						height={1000}
 						alt='donald'
