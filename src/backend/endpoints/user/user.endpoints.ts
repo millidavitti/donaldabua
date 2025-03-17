2fd826385099ef6cfdafbbea5296b6cefc238940
davitti <abuadonald@gmail.com>
2025-03-17 02:30:15 +0100

    refactor(endpoints): remove BASE_URL and use env variable directly
    - Replaced BASE_URL with process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT
    - Removed unnecessary import for cleaner and more direct endpoint definitions

--- src/backend/endpoints/user/user.endpoints.ts
+++ src/backend/endpoints/user/user.endpoints.ts
@@ -1,9 +1,10 @@
-import { BASE_URL } from "..";
-
 export const USER_ENDPOINTS = {
-	list: `${BASE_URL}/users/public`,
-	read: (userId: string) => `${BASE_URL}/users/public/${userId}`,
-	create: `${BASE_URL}/users/private`,
-	update: (userId: string) => `${BASE_URL}/users/private/${userId}`,
-	delete: (userId: string) => `${BASE_URL}/users/private/${userId}`,
+	list: `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/users/public`,
+	read: (userId: string) =>
+		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/users/public/${userId}`,
+	create: `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/users/private`,
+	update: (userId: string) =>
+		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/users/private/${userId}`,
+	delete: (userId: string) =>
+		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/users/private/${userId}`,
 };
