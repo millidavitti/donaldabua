3846e24b0f8d14af48d6afc71a1d2fd8410972e2
davitti <abuadonald@gmail.com>
2025-03-17 03:43:43 +0100

    ✨ feat(dashboard): add dashboard constants for views and settings
    - create DASHBOARD_VIEWS for various dashboard editing options
    - define PROJECT_DRAFT_VIEWS for project draft management
    - add AVAILABILITY_OPTIONS for user availability preferences
    - include SETTINGS_VIEWS for managing technology settings
    - list SOCIAL_PLATFORMS with associated icons for social media
    - introduce DELAY constant for animation timing
    - define ANIMATIONS for UI effects and types

---
+++ src/data/dashboard-data/dasboard-constants.ts
@@ -0,0 +1,79 @@
+export const DASHBOARD_VIEWS = [
+	"edit-image",
+	"edit-location",
+	"edit-name",
+	"edit-video",
+	"edit-hours-per-week",
+	"edit-title",
+	"edit-hourly-rate",
+	"edit-profile-overview",
+	"edit-profile-technologies",
+	"edit-employment-history",
+	"add-socials",
+	"edit-portfolio",
+	"edit-published-project",
+	"update-socials",
+	"view-project",
+	"create-profile",
+	"select-profile",
+	"settings",
+	"alert-dialog",
+	null,
+] as const;
+
+export const PROJECT_DRAFT_VIEWS = [
+	"edit-project-video",
+	"edit-project-image",
+	"edit-project-markdown",
+	null,
+] as const;
+
+export const AVAILABILITY_OPTIONS = [
+	"More than 30 hrs/week",
+	"Less than 30 hrs/week",
+	"As needed - open to offers",
+	"None",
+] as const;
+
+export const SETTINGS_VIEWS = ["manage-technologies", null] as const;
+
+export const SOCIAL_PLATFORMS = [
+	"Facebook",
+	"Instagram",
+	"X / Twitter",
+	"TikTok",
+	"LinkedIn",
+	"Snapchat",
+	"Pinterest",
+	"Reddit",
+	"YouTube",
+	"WhatsApp",
+	"Telegram",
+	"Discord",
+	"WeChat",
+	"Threads",
+	"Tumblr",
+] as const;
+
+export const SOCIAL_PLATFORM_ICONS = {
+	Facebook: "/social-media-icons/facebook.svg",
+	Instagram: "/social-media-icons/instagram.svg",
+	"X / Twitter": "/social-media-icons/twitter.svg",
+	TikTok: "/social-media-icons/tik-tok.svg",
+	LinkedIn: "/social-media-icons/linkedin.svg",
+	Snapchat: "/social-media-icons/snapchat.svg",
+	Pinterest: "/social-media-icons/pinterest.svg",
+	Reddit: "/social-media-icons/reddit.svg",
+	YouTube: "/social-media-icons/youtube.svg",
+	WhatsApp: "/social-media-icons/whatsapp.svg",
+	Telegram: "/social-media-icons/telegram.svg",
+	Discord: "/social-media-icons/discord.svg",
+	WeChat: "/social-media-icons/wechat.svg",
+	Threads: "/social-media-icons/threads.svg",
+	Tumblr: "/social-media-icons/tumblr.svg",
+} as const;
+
+export const DELAY = 200;
+
+export const ANIMATIONS = ["neonScan", "swing-in-top-fwd"] as const;
+export type AnimationClass = (typeof ANIMATIONS)[number];
