47c3eb697d64d1d4c233bd86b71de09fd8cd5fbe
davitti <abuadonald@gmail.com>
2025-03-17 08:09:54 +0100

    ✨ feat(home): add project vault component
    - create ProjectVault component to manage user projects
    - integrate sidebar and profile sections for better UX
    - utilize Jotai for state management of user data

---
+++ src/app/(home)/components/project-vault.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Flex from "@/components/layouts/flex";
+import VaultHeader from "./vault-header";
+import User from "./user/user";
+import Sidebar from "./sidebar";
+import ProfileSummary from "./profile/profile-summary";
+import ProfileProjects from "./profile/profile-projects";
+import ProfileTechnologies from "./profile/profile-technologies";
+import SelectUser from "./select-user";
+import { useAtomValue } from "jotai";
+import { cn } from "@/utils/cn";
+import { getAnimationClass } from "@/utils/animations";
+import { user_snapshot_jotai } from "@/data/home/home-atoms/home-data";
+
+export default function ProjectVault() {
+	const user_snapshot = useAtomValue(user_snapshot_jotai);
+	return (
+		<>
+			{Boolean(user_snapshot.id) && (
+				<Flex
+					className={cn("w-full h-full gap-3", getAnimationClass("neonScan"))}
+					flex='column'
+				>
+					<VaultHeader />
+					<Flex className='outline h-full gap-3' flex='column'>
+						<User />
+						{/* Main */}
+						<Flex className='grow gap-3 shrink-0 flex-wrap h-fit'>
+							<Sidebar />
+							<Flex className='basis-[720px] grow-[2] gap-3' flex='column'>
+								<ProfileSummary />
+								<ProfileProjects />
+								<ProfileTechnologies />
+							</Flex>
+						</Flex>
+					</Flex>
+				</Flex>
+			)}
+			<SelectUser />
+		</>
+	);
+}
