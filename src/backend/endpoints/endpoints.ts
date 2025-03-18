aa4ba9febf6ad56f359b50b676f88b08a4667d27
davitti <abuadonald@gmail.com>
2025-03-18 21:24:15 +0100

    ♻️ refactor(endpoints): standardize endpoint imports and structure
    - rename endpoint variables to singular form for consistency
    - add new project-related endpoints for enhanced functionality

--- src/backend/endpoints/endpoints.ts
+++ src/backend/endpoints/endpoints.ts
@@ -1,17 +1,23 @@
-import { PING_ENDPOINTS } from "./ping.endpoints";
-import { PROFILE_ENDPOINTS } from "./profile/profile.endpoint";
-import { LOCATION_ENDPOINTS } from "./user/location.endpoints";
-import { SOCIALS_ENDPOINTS } from "./user/socials.endpoint";
-import { USER_ENDPOINTS } from "./user/user.endpoints";
-import { PROFILE_TECHNOLOGY_ENDPOINTS } from "./profile/profile-technology.endpoint";
-import { PROJECT_ENDPOINTS } from "./project/project.endpoint";
+import { PING_ENDPOINT } from "./ping.endpoints";
+import { PROFILE_ENDPOINT } from "./profile/profile.endpoint";
+import { LOCATION_ENDPOINT } from "./user/location.endpoints";
+import { SOCIALS_ENDPOINT } from "./user/socials.endpoint";
+import { USER_ENDPOINT } from "./user/user.endpoints";
+import { PROFILE_TECHNOLOGY_ENDPOINT } from "./profile/profile-technology.endpoint";
+import { PROJECT_ENDPOINT } from "./project/project.endpoint";
+import { PROJECT_CONTENT_ENDPOINT } from "./project/project-content.endpoint";
+import { PROJECT_TECHNOLOGY_ENDPOINT } from "./project/project-technology.endpoint";
+import { TECHNOLOGY_ENDPOINT } from "./technology.endpoint";
 
 export const ENDPOINTS = {
-	user: USER_ENDPOINTS,
-	location: LOCATION_ENDPOINTS,
-	socials: SOCIALS_ENDPOINTS,
-	ping: PING_ENDPOINTS,
-	profile: PROFILE_ENDPOINTS,
-	profileTechnology: PROFILE_TECHNOLOGY_ENDPOINTS,
-	project: PROJECT_ENDPOINTS,
+	user: USER_ENDPOINT,
+	location: LOCATION_ENDPOINT,
+	socials: SOCIALS_ENDPOINT,
+	ping: PING_ENDPOINT,
+	profile: PROFILE_ENDPOINT,
+	profileTechnology: PROFILE_TECHNOLOGY_ENDPOINT,
+	project: PROJECT_ENDPOINT,
+	projectContent: PROJECT_CONTENT_ENDPOINT,
+	projectTechnology: PROJECT_TECHNOLOGY_ENDPOINT,
+	technology: TECHNOLOGY_ENDPOINT,
 };
