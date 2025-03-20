25b24825cdc720428f4361707924a1c018813cef
davitti <abuadonald@gmail.com>
2025-03-20 23:35:55 +0100

    ✨ feat(home): replace ProjectVault with SelectUser component
    - update page component to render SelectUser instead of ProjectVault
    - improve user interaction by selecting a user on the home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ProjectVault from "./components/project-vault";
 import { Metadata } from "next";
+import SelectUser from "./components/select-user";
 
 export const metadata: Metadata = {
 	title: "Donald Abua",
 };
-export default function page() {
-	return <ProjectVault />;
+export default function SelectUserPage() {
+	return <SelectUser />;
 }
