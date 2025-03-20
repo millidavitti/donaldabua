a3a77fffc740ac32175fdc40cc039c6fd9b0fc6f
davitti <abuadonald@gmail.com>
2025-03-20 23:38:42 +0100

    🔧 chore(auth): remove unused signOut function
    - delete signOut function as it is no longer needed
    - clean up the codebase to improve maintainability

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -19,25 +19,6 @@ export async function auth(request?: NextRequest) {
 	}
 }
 
-export async function signOut() {
-	try {
-		const res = await fetch(
-			process.env.NEXT_PUBLIC_AUTH_ENDPOINT + "/sign-out",
-			{
-				method: "GET",
-				credentials: "include",
-			},
-		);
-		const { error, isAuthenticated } = await res.json();
-		console.log({ error, isAuthenticated });
-		if (error) throw new Error(error);
-		else if (isAuthenticated) return { isAuthenticated };
-	} catch (error) {
-		console.error("---signOut---", error);
-		throw new Error(getErrorMessage(error));
-	}
-}
-
 type auth = {
 	user?: { id: string; expires: string };
 	error?: string;
