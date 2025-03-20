d718c660435dcc70faba9f84c94988a2107ac23b
davitti <abuadonald@gmail.com>
2025-03-20 23:37:39 +0100

    ♻️ refactor(select-user): simplify user selection method
    - remove unused atom setter for user snapshot
    - replace button click with link for direct user profile navigation
    - clean up imports to only include necessary atoms

--- src/app/(home)/components/select-user.tsx
+++ src/app/(home)/components/select-user.tsx
@@ -1,14 +1,11 @@
 "use client";
 import Flex from "@/components/layouts/flex";
 import Button from "@/components/ui/button";
-import {
-	user_snapshot_jotai,
-	users_snapshot_jotai,
-} from "@/data/home/home-atoms/home-data";
-import { useAtomValue, useSetAtom } from "jotai";
+import { users_snapshot_jotai } from "@/data/home/home-atoms/home-data";
+import { useAtomValue } from "jotai";
+import Link from "next/link";
 
 export default function SelectUser() {
-	const user_snapshot_setter = useSetAtom(user_snapshot_jotai);
 	const users_snapshot = useAtomValue(users_snapshot_jotai);
 
 	return (
@@ -20,13 +17,9 @@ export default function SelectUser() {
 				Welcome, select a profile to get started
 			</h2>
 			{users_snapshot.map((user) => (
-				<Button
-					type='button'
-					key={user.id}
-					onClick={() => user_snapshot_setter(user)}
-				>
-					{user.name}
-				</Button>
+				<Link href={user.id} key={user.id}>
+					<Button type='button'>{user.name}</Button>
+				</Link>
 			))}
 		</Flex>
 	);
