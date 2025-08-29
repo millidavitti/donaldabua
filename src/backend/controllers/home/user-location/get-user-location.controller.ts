bded8a859fe4913244e229f9de62b87e25c36d63
davitti <abuadonald@gmail.com>
2025-08-29 14:08:12 +0100

    refactor: simplify `getUserLocationController` by removing CSRF handling and userId param

--- src/backend/controllers/home/user-location/get-user-location.controller.ts
+++ src/backend/controllers/home/user-location/get-user-location.controller.ts
@@ -1,25 +1,15 @@
-import { APIResponse, UserLocation } from "@/data/home/home-atoms/home-data";
-import { getErrorMessage } from "@/utils/get-error-message";
-import { generateCsrfToken } from "../../../auth/get-csrf-token.controller";
 import { ENDPOINTS } from "@/backend/endpoints/endpoints";
+import { generateErrorLog } from "@/utils/generate-error-log";
 
-export async function getUserLocationController(userId: string) {
-	const headers = new Headers();
-
+export async function getUserLocationController() {
 	try {
-		const { error, csrfToken } = await generateCsrfToken();
-		if (error) throw new Error(error);
-		else if (csrfToken) headers.append("x-csrf-token", csrfToken);
-
-		const res = await fetch(ENDPOINTS.location.getLocation(userId), {
+		const res = await fetch(ENDPOINTS.location.getLocation(), {
 			credentials: "include",
-			headers,
 		});
-		const data = await res.json();
+		const json = await res.json();
 
-		return data as APIResponse<UserLocation, "location">;
+		return json;
 	} catch (error) {
-		console.error("---getUserLocationController---\n", error);
-		throw new Error(getErrorMessage(error));
+		generateErrorLog("@getUserLocationController", error);
 	}
 }
