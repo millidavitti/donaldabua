8e853f065f9072b5e0a05073081ff664606273e0
davitti <abuadonald@gmail.com>
2025-08-29 01:02:33 +0100

    refactor(user-name): switch from jotai atom to local state in useEditUserNameInterface

--- src/hooks/interface/dashboard/use-edit-user-name-interface.ts
+++ src/hooks/interface/dashboard/use-edit-user-name-interface.ts
@@ -1,19 +1,14 @@
-import { updateUserController } from "@/backend/controllers/dashboard/user/update-user.controller";
-import {
-	user_snapshot_jotai,
-	user_name_jotai,
-} from "@/data/dashboard/dashboard-atoms/dashboard-data";
 import { dashboard_view_jotai } from "@/data//dashboard/dashboard-atoms/dashboard-ui-state";
 import { useAtom } from "jotai";
-import { toast } from "sonner";
 import {
 	mutate_user_atom,
 	payload_view_atom,
 } from "@/data/dashboard/dashboard-atoms/data";
+import { useState } from "react";
 
 export function useEditUserNameInterface() {
 	const [dashboard_view, dashboard_view_setter] = useAtom(dashboard_view_jotai);
-	const [user_name, user_name_setter] = useAtom(user_name_jotai);
+	const [userName, setUserName] = useState("");
 	const [mutate_user] = useAtom(mutate_user_atom);
 	const [payload_view] = useAtom(payload_view_atom);
 
@@ -21,17 +16,16 @@ export function useEditUserNameInterface() {
 		dashboard_view_setter("edit-name");
 	}
 	function cancelNameEdit() {
-		user_name_setter(payload_view.data?.user.name);
 		dashboard_view_setter(null);
 	}
 
 	async function saveNameEdit() {
 		dashboard_view_setter(null);
-		mutate_user.mutateAsync({ name: user_name });
+		mutate_user.mutateAsync({ name: userName });
 	}
 
 	function captureNameEdit(value: string) {
-		user_name_setter(value);
+		setUserName(value);
 	}
 	return {
 		edit_profile: dashboard_view,
