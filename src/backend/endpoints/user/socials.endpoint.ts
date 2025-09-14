80cc9164e44ddcc474a0bde800b60d9a1ffeff9d
davitti <abuadonald@gmail.com>
2025-09-14 02:45:18 +0100

    refactor: simplify socials creation flow with `createSocials` and `/socials/me` endpoint, add error logging

--- src/backend/endpoints/user/socials.endpoint.ts
+++ src/backend/endpoints/user/socials.endpoint.ts
@@ -1,8 +1,7 @@
 export const SOCIALS_ENDPOINT = {
 	list: (userId: string) =>
 		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/socials/${userId}`,
-	create: (userId: string) =>
-		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/socials/private/${userId}`,
+	create: () => `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/socials/me`,
 	update: (socialAccountId: string) =>
 		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/socials/private/${socialAccountId}`,
 	delete: (socialAccountId: string) =>
