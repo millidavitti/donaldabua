563f5ec9fa2ec85aad6a32b2df95814cd687334a
davitti <abuadonald@gmail.com>
2025-09-16 15:46:26 +0100

    refactor(profile): simplify update & delete controllers
    - switched `updateProfile` to use `PATCH` instead of `PUT`
    - removed CSRF token handling and unused `APIResponse` typing
    - changed endpoints to `/profiles/me` for both update and delete
    - `deleteProfile` now accepts partial `Profile` in body instead of ID
    - added `generateErrorLog` for error handling

--- src/backend/endpoints/profile/profile.endpoint.ts
+++ src/backend/endpoints/profile/profile.endpoint.ts
@@ -2,8 +2,6 @@ export const PROFILE_ENDPOINT = {
 	list: (userId: string) =>
 		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/${userId}`,
 	create: () => `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/me`,
-	update: (profileId: string) =>
-		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/private/${profileId}`,
-	delete: (profileId: string) =>
-		`${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/private/${profileId}`,
+	update: () => `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/me`,
+	delete: () => `${process.env.NEXT_PUBLIC_BACKEND_API_ENDPOINT}/profiles/me`,
 };
