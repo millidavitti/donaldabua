14501b4a7945f1e3b2cdcbf49343fc2ec24aefcc
davitti <abuadonald@gmail.com>
2025-09-23 01:01:31 +0100

    refactor(home): switch project content to shared dashboard atom
    - replaced `project_content_jotai` with `input_project_content_atom`
    - aligns project content state between home and dashboard views
    - simplifies state management by reusing shared atoms

--- src/app/(home)/components/project/project-content.tsx
+++ src/app/(home)/components/project/project-content.tsx
@@ -3,11 +3,11 @@ import ContentImage from "../content/content-image";
 import { createId } from "@paralleldrive/cuid2";
 import ContentBuilderVideo from "../content/content-video";
 import ContentMarkdown from "../content/content-markdown";
-import { project_content_jotai } from "@/data/home/home-atoms/home-data";
+import { input_project_content_atom } from "@/data/dashboard/dashboard-atoms/data";
 import { useAtomValue } from "jotai";
 
 export default function ProjectContent() {
-	const project_content = useAtomValue(project_content_jotai);
+	const project_content = useAtomValue(input_project_content_atom);
 	return (
 		<Flex flex='column' className='basis-[360px] grow-[2] gap-3 border-0 p-0'>
 			{project_content
