de157bf21629d2f51502f34a8b9ad22e6a77bb64
davitti <abuadonald@gmail.com>
2025-10-20 18:37:38 +0100

    refactor(edit-profile): improve layout for availability and projects sections
    - removed unused `id="availability"` wrapper to simplify DOM
    - swapped import from `published-project/projects` to `project/projects`
    - converted projects header to sticky for better UX inside scrollable container
    - added height and overflow auto to enable proper scrolling

--- src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
+++ src/app/(dashboard)/components/edit-profile/edit-profile-projects.tsx
@@ -2,7 +2,7 @@
 import Flex from "@/components/layouts/flex";
 import InteractiveIcon from "@/components/layouts/interactive_icon";
 import { Plus } from "lucide-react";
-import Projects from "../published-project/projects";
+import Projects from "../project/projects";
 import { useEditProjects } from "@/hooks/interface/dashboard/use-edit-projects.interface";
 
 export default function EditProjects() {
@@ -13,19 +13,16 @@ export default function EditProjects() {
 			{Modal}
 			<Flex
 				flex='column'
-				className='gap-3 border-0 p-0'
-				htmlProps={{ id: "projects" }}
+				className='gap-3 border-0 relative p-0 h-[540px] overflow-y-auto'
 			>
-				<a href='#projects'>
-					{/* Header */}
-					<Flex className='items-center justify-between shrink-0'>
+				<a href='#projects' className='sticky top-0 z-10 shrink-0'>
+					<Flex className='items-center justify-between bg-light-surface shrink-0'>
 						<p className='font-semibold lg:text-2xl'>Projects</p>
 						<InteractiveIcon callback={start}>
 							<Plus size={24} />
 						</InteractiveIcon>
 					</Flex>
 				</a>
-				{/* Projects */}
 				<Projects />
 			</Flex>
 		</>
