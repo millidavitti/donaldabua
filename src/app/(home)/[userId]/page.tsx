5e67f35511311f2377edb5ee0e07e78e41477c0d
davitti <abuadonald@gmail.com>
2025-10-21 20:02:20 +0100

    refactor(home): migrate public profile page to shared dashboard profile components
    - removed ProfileSummary, ProfileProjects, and ProfileTechnologies usage
    - replaced title/hourly/overview with shared dashboard render-prop editors in read-only mode
    - switched project listing to reused <Projects /> component
    - reduces duplication between dashboard and public profile pages

--- src/app/(home)/[userId]/page.tsx
+++ src/app/(home)/[userId]/page.tsx
@@ -7,12 +7,13 @@ import { user_snapshot_jotai } from "@/data/home/home-atoms/home-data";
 import VaultHeader from "../components/vault-header";
 import User from "../components/user/user";
 import Sidebar from "../components/sidebar";
-import ProfileSummary from "../components/profile/profile-summary";
-import ProfileProjects from "../components/profile/profile-projects";
-import ProfileTechnologies from "../components/profile/profile-technologies";
 import { useParams } from "next/navigation";
 import { useEffect } from "react";
 import { getUserController } from "@/backend/controllers/home/user/get-user.controller";
+import Projects from "@/app/(dashboard)/components/project/projects";
+import HourlyRate from "@/app/(dashboard)/components/edit-profile/edit-profile-hourly-rate";
+import Overview from "@/app/(dashboard)/components/edit-profile/edit-profile-overview";
+import ProfileTitle from "@/app/(dashboard)/components/edit-profile/edit-profile-title";
 
 export default function ProjectVault() {
 	const user_snapshot_setter = useSetAtom(user_snapshot_jotai);
@@ -41,9 +42,22 @@ export default function ProjectVault() {
 							className='basis-[720px] grow-[2] border-0 p-0 gap-3'
 							flex='column'
 						>
-							<ProfileSummary />
-							<ProfileProjects />
-							<ProfileTechnologies />
+							<Flex flex='column' className='p-0 gap-3 border-0'>
+								<Flex className='shrink-0 justify-between flex-wrap md:flex-nowrap gap-3 outline-0'>
+									<ProfileTitle />
+									<HourlyRate />
+								</Flex>
+								<Overview />
+							</Flex>
+							<Flex flex='column' className='border-0 p-0 gap-3'>
+								<a href='#projects'>
+									{/* Header */}
+									<Flex className='items-center justify-between shrink-0'>
+										<p className='font-semibold lg:text-2xl'>Projects</p>
+									</Flex>
+								</a>
+								<Projects />
+							</Flex>
 						</Flex>
 					</Flex>
 				</Flex>
