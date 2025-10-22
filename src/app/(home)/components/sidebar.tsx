4aecd6e01839ef04afaa949c4bc943a577ddf4fc
davitti <abuadonald@gmail.com>
2025-10-22 18:02:05 +0100

    refactor(home-sidebar): migrate socials to slot-based API
    - replaced render-prop usage with `slot='link'` pattern
    - removed `SocialsHeader` import and animation delay logic
    - simplified layout and reduced nesting

--- src/app/(home)/components/sidebar.tsx
+++ src/app/(home)/components/sidebar.tsx
@@ -3,38 +3,21 @@ import Flex from "@/components/layouts/flex";
 
 import IntroVideo from "@/app/(dashboard)/components/edit-user/edit-intro-video";
 import Availability from "@/app/(dashboard)/components/edit-profile/edit-availability";
-import Socials from "@/app/(dashboard)/components/edit-user/edit-socials";
-import Social from "@/app/(dashboard)/components/edit-user/social";
-import SocialsHeader from "@/app/(dashboard)/components/edit-user/socials-header";
-import { DELAY } from "@/data/home/home-constants";
+import Socials, {
+	Social,
+} from "@/app/(dashboard)/components/edit-user/edit-socials";
 
 export default function Sidebar() {
 	return (
 		<Flex
 			flex='column'
-			className='grow lg:shrink-0 lg:basis-80 p-0 border-0 gap-3'
+			className='gap-3 p-0 border-0 grow lg:shrink-0 lg:basis-80'
 		>
 			<IntroVideo />
-			<Flex flex='column' className='grow p-0 border-0 gap-3'>
+			<Flex flex='column' className='gap-3 p-0 border-0 grow'>
 				<Availability />
 				<Socials>
-					{(socials) => (
-						<>
-							<SocialsHeader />
-							{/* Socials*/}
-							<Flex flex='column' className='gap-3 border-0 p-0'>
-								{socials?.map((social, i) => (
-									<a
-										key={social.id}
-										href={social.profile}
-										style={{ animationDelay: i * DELAY + "ms" }}
-									>
-										<Social social={social} />
-									</a>
-								))}
-							</Flex>
-						</>
-					)}
+					<Social slot='link' />
 				</Socials>
 			</Flex>
 		</Flex>
