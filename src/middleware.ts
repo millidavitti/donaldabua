b458e066ac5b2e8d06cedac9bbc69534f0589e61
davitti <abuadonald@gmail.com>
2025-10-26 04:17:26 +0100

    refactor: simplify middleware config matcher regex

--- src/middleware.ts
+++ src/middleware.ts
@@ -67,7 +67,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: [
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-	],
+	matcher: ["/((?!_next|server|api|.*\\..*).*)"],
 };
